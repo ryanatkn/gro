560fa38429210cee060afa9e8ff492989775dbeb
Ryan Atkinson <ryanatkn@gmail.com>
2020-03-16 15:05:14 -0500

    improve oki tests for execution order

--- src/oki/oki.test.ts
+++ src/oki/oki.test.ts
@@ -10,109 +10,70 @@ test('test()', t => {
 		t.equal({math: 2 + 2}, {math: 4});
 	});
 
+	// see ./assertions.test.ts for more
+
 	test('nested1', () => {
 		test('nested2', () => {
-			test('nested3a', () => {
-				test('nested4a', () => {
-					t.is(4, 4);
-				});
-				test('nested4b', () => {
-					t.is(4, 4);
-				});
-			});
-			test('nested3b', () => {});
+			t.is('nested', 'nested');
 		});
 	});
 
-	test('sync1', () => {
-		t.is(8, 8);
-		test('sync2a', () => {
-			t.is(8, 8);
-			test('sync3a', () => {
-				t.is(8, 8);
-				test('sync4a', () => {
-					t.is(8, 8);
-				});
-				t.is(8, 8);
-				test('sync4b', () => {
-					t.is(8, 8);
-				});
-				t.is(8, 8);
-			});
-			t.is(8, 8);
-			test('sync3b', () => {
-				t.is(8, 8);
-			});
-			t.is(8, 8);
-		});
-		test('sync2b', () => {
-			t.is(8, 8);
-		});
-		test('sync2c', () => {
-			t.is(8, 8);
-			test('sync3a', () => {
-				t.is(8, 8);
-			});
-		});
-	});
-
-	let count = 0;
-	test('sync execution order1', () => {
-		t.is(count, 0);
-		count++;
+	test('sync execution order', () => {
+		let syncCount = 0;
 		test('sync execution order2a', () => {
-			t.is(count, 1);
-			count++;
+			t.is(syncCount++, 1);
 			test('sync execution order3a', () => {
-				t.is(count, 2);
-				count++;
+				t.is(syncCount++, 3);
 			});
 			test('sync execution order3b', () => {
-				t.is(count, 3);
-				count++;
+				t.is(syncCount++, 4);
 				test('sync execution order4', () => {
-					t.is(count, 4);
-					count++;
+					t.is(syncCount++, 6);
 				});
-				t.is(count, 4);
+				t.is(syncCount++, 5);
 			});
-			t.is(count, 2);
+			t.is(syncCount++, 2);
 		});
 		test('sync execution order2b', () => {
-			t.is(count, 5);
-			count++;
+			t.is(syncCount++, 7);
 		});
 		test('sync execution order2c', () => {
-			t.is(count, 6);
+			t.is(syncCount++, 8);
 		});
-		t.is(count, 1);
+		t.is(syncCount++, 0);
 	});
-	t.is(count, 0);
 
-	let waitValue1 = 1;
-	let waitValue2 = 1;
-	test('async', async () => {
+	test('async execution order', async () => {
+		let asyncCount = 0;
+		test('async execution order2a', async () => {
+			await wait();
+			t.is(asyncCount++, 1);
+			test('async execution order3a', async () => {
+				await wait();
+				t.is(asyncCount++, 3);
+			});
+			test('async execution order3b', async () => {
+				await wait();
+				t.is(asyncCount++, 4);
+				test('async execution order4', async () => {
+					await wait();
+					t.is(asyncCount++, 6);
+				});
+				await wait();
+				t.is(asyncCount++, 5);
+			});
+			await wait();
+			t.is(asyncCount++, 2);
+		});
+		test('async execution order2b', async () => {
+			await wait();
+			t.is(asyncCount++, 7);
+		});
+		test('async execution order2c', async () => {
+			await wait();
+			t.is(asyncCount++, 8);
+		});
 		await wait();
-		test('nested sync test runs after parent completes', () => {
-			t.is(waitValue1, 2);
-		});
-		test('nested async test runs after parent completes', async () => {
-			t.is(waitValue1, 2);
-		});
-		test('nested async test with delay runs after parent completes', async () => {
-			await wait();
-			t.is(waitValue1, 2);
-		});
-		await test('awaited nested async test with delay runs after parent completes', async () => {
-			await wait();
-			t.is(waitValue1, 2);
-			waitValue2 = 2;
-		});
-		t.is(waitValue1, 1);
-		waitValue1 = 2;
-	});
-	test('runs after async', () => {
-		t.is(waitValue1, 2);
-		t.is(waitValue2, 2);
+		t.is(asyncCount++, 0);
 	});
 });
