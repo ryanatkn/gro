139a75ea2e07e7d7a754fda0635fd5a85f0d9a88
Ryan Atkinson <ryanatkn@gmail.com>
2020-04-02 20:44:01 -0500

    use inferred types in task examples

--- src/run/fixtures/testTask1.task.ts
+++ src/run/fixtures/testTask1.task.ts
@@ -1,7 +1,7 @@
-import {Task, TaskContext} from '../task.js';
+import {Task} from '../task.js';
 
 export const task: Task = {
-	run: async ({log: {info}}: TaskContext): Promise<void> => {
+	run: async ({log: {info}}) => {
 		info('test task 1!');
 	},
 };
