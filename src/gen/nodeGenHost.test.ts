fecf8cfa99814dfda7b85011d9a876d3a81f85e2
Ryan Atkinson <ryanatkn@gmail.com>
2020-04-11 17:30:52 -0500

    clean up nodeGenHost tests a bit

--- src/gen/nodeGenHost.test.ts
+++ src/gen/nodeGenHost.test.ts
@@ -1,4 +1,4 @@
-import {resolve} from 'path';
+import {resolve, join} from 'path';
 
 import {test} from '../oki/index.js';
 import {createNodeGenHost} from './nodeGenHost.js';
@@ -6,21 +6,21 @@ import {validateGenModule} from './gen.js';
 
 test('createNodeGenHost()', t => {
 	const host = createNodeGenHost({logLevel: 0});
+	const fixturesDir = resolve('src/gen/fixtures');
 
 	test('host.findGenModules()', async () => {
-		const genSourceIds = await host.findGenModules(resolve('src/gen/fixtures'));
+		const genSourceIds = await host.findGenModules(fixturesDir);
 		t.equal(genSourceIds, [
-			resolve('src/gen/fixtures/testGenHtml.gen.html.ts'),
-			resolve('src/gen/fixtures/testGenMulti.gen.ts'),
-			resolve('src/gen/fixtures/testGenTs.gen.ts'),
+			join(fixturesDir, 'testGenHtml.gen.html.ts'),
+			join(fixturesDir, 'testGenMulti.gen.ts'),
+			join(fixturesDir, 'testGenTs.gen.ts'),
 		]);
 	});
 
 	test('host.loadGenModule()', async () => {
-		const gen = await host.loadGenModule(
-			resolve('src/gen/fixtures/testGenTs.gen.ts'),
-		);
-		t.is(gen.id, resolve('src/gen/fixtures/testGenTs.gen.ts'));
+		const id = join(fixturesDir, 'testGenTs.gen.ts');
+		const gen = await host.loadGenModule(id);
+		t.is(gen.id, id);
 		t.ok(validateGenModule(gen.mod));
 	});
 });
