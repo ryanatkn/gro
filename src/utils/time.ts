23de0a65d6c4ec4a47da24fef3be64d2e5d38f1d
Ryan Atkinson <ryanatkn@gmail.com>
2020-04-11 19:40:37 -0500

    change Timings api to have a default key

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -15,19 +15,25 @@ export const createStopwatch = (decimals = 2): Stopwatch => {
 	};
 };
 
+const DEFAULT_KEY = Symbol();
+
 export class Timings {
 	private readonly timings = new Map<any, number>();
 	private readonly stopwatches = new Map<any, Stopwatch>();
 
 	constructor(public readonly decimals?: number) {}
 
-	start(key: any, replace = false, decimals = this.decimals): void {
+	start(
+		key: any = DEFAULT_KEY,
+		replace = false,
+		decimals = this.decimals,
+	): void {
 		if (!replace && this.stopwatches.has(key)) {
 			throw Error(`Timing key is already in use: ${key}`);
 		}
 		this.stopwatches.set(key, createStopwatch(decimals));
 	}
-	stop(key: any): number {
+	stop(key: any = DEFAULT_KEY): number {
 		const stopwatch = this.stopwatches.get(key);
 		if (!stopwatch) {
 			throw Error(`Unknown timing key cannot be stopped: ${key}`);
@@ -37,7 +43,7 @@ export class Timings {
 		this.timings.set(key, timing);
 		return timing;
 	}
-	get(key: any): number {
+	get(key: any = DEFAULT_KEY): number {
 		const timing = this.timings.get(key);
 		if (timing === undefined) {
 			throw new Error(`Timing key not found: ${key}`);
