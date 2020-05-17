7b96b35df7ebd24daf204dd4376745452d765e51
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-17 14:33:01 -0500

    add a note about a future improvement

--- src/utils/createObtainable.ts
+++ src/utils/createObtainable.ts
@@ -11,6 +11,9 @@ The motivating use case was reusing a database connection across multiple tasks.
 
 See the tests for usage examples - ./createObtainable.test.ts
 
+A future improvement could have `release` always return a promise
+that resolves when the obtainable is fully released.
+
 */
 export const createObtainable = <T>(
 	obtain: () => T,
