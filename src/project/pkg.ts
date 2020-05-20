850fb4e402c872dfbdd969120f20071c7791768b
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-20 13:49:48 -0500

    change comment formatting

--- src/project/pkg.ts
+++ src/project/pkg.ts
@@ -4,9 +4,13 @@ import {readJsonSync} from '../fs/nodeFs.js';
 import {paths, groPaths} from '../paths.js';
 import {Json} from '../utils/json.js';
 
-// This is a single entrypoint for getting the `packge.json` of both the current project and Gro.
-// It's helpful because Node's ES modules do not yet support json files without a flag,
-// and in the future we should be able to easily auto-generate types for them.
+/*
+
+This is a single entrypoint for getting the `packge.json` of both the current project and Gro.
+It's helpful because Node's ES modules do not yet support json files without a flag,
+and in the future we should be able to easily auto-generate types for them.
+
+*/
 
 type PackageJson = Obj<Json>; // TODO generate one day
 type GroPackageJson = Obj<Json>; // TODO generate one day
