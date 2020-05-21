d01d5f0ec7b08f3781402bfd1666b8dcd554c6db
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-21 13:49:55 -0500

    improve the log message and comments

--- src/cli/invoke.ts
+++ src/cli/invoke.ts
@@ -93,9 +93,9 @@ const main = async () => {
 			// First ensure that the project has been built.
 			// This is useful for initial project setup and CI.
 			if (!(await pathExists(toImportId(pathData.id)))) {
-				log.info('Building the project for initial setup.');
+				log.info('Task file not found in build directory. Compiling TypeScript...');
 				subTimings.start('build project');
-				await spawnProcess('node_modules/.bin/tsc');
+				await spawnProcess('node_modules/.bin/tsc'); // ignore compiler errors
 				subTimings.stop('build project');
 			}
 
