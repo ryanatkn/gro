595a957007013b15deb37a01ad3f1d8b4f2f3693
Ryan Atkinson <ryanatkn@gmail.com>
2020-10-07 22:24:18 -0500

    update dev task

--- src/dev.task.ts
+++ src/dev.task.ts
@@ -2,16 +2,24 @@ import {Task} from './task/task.js';
 import {Filer} from './build/Filer.js';
 import {createDevServer} from './devServer/devServer.js';
 import {createDefaultCompiler} from './compile/defaultCompiler.js';
-import {paths} from './paths.js';
-import {loadBuildConfigs} from './build/buildConfig.js';
+import {paths, toBuildOutDir} from './paths.js';
+import {loadBuildConfigs, loadPrimaryBuildConfig} from './build/buildConfig.js';
 
 export const task: Task = {
 	description: 'start development server',
 	run: async (): Promise<void> => {
+		const buildConfigs = await loadBuildConfigs();
+		const primaryBuildConfig = await loadPrimaryBuildConfig();
+		const buildConfigToServe =
+			primaryBuildConfig.platform === 'browser'
+				? primaryBuildConfig
+				: buildConfigs.find((c) => c.platform === 'browser') || primaryBuildConfig;
+		const buildOutDirToServe = toBuildOutDir(true, buildConfigToServe.name, '');
 		const filer = new Filer({
 			compiler: createDefaultCompiler(),
-			compiledDirs: [paths.source],
-			buildConfigs: await loadBuildConfigs(),
+			compiledDirs: [paths.source, paths.externals],
+			servedDirs: [buildOutDirToServe, {dir: paths.externals, servedAt: paths.build}],
+			buildConfigs,
 		});
 
 		const devServer = createDevServer({filer});
