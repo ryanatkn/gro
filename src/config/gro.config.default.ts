d2988f81b83d4cc71499dae184aad5d22519bf48
Ryan Atkinson <ryanatkn@gmail.com>
2020-10-19 23:27:07 -0500

    improve the default gro config

--- src/config/gro.config.default.ts
+++ src/config/gro.config.default.ts
@@ -1,10 +1,20 @@
+import {createFilter} from '@rollup/pluginutils';
+
+import {basePathToSourceId} from '../paths.js';
 import {GroConfigCreator, PartialGroConfig} from './config.js';
 
 // This is the default config that's used if the current project does not define one.
 
 const createConfig: GroConfigCreator = async () => {
 	const config: PartialGroConfig = {
-		builds: [{name: 'browser', platform: 'browser'}],
+		builds: [
+			{name: 'browser', platform: 'browser', dist: true},
+			{
+				name: 'node',
+				platform: 'node',
+				include: createFilter(undefined, basePathToSourceId('**/*.svelte')),
+			}, // TODO include only tasks and such
+		],
 	};
 
 	return config;
