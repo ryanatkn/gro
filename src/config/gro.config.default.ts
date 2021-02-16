a11e6327bf7669bc200638c225610a833ddbb65b
Ryan Atkinson <ryanatkn@gmail.com>
2021-02-16 09:50:37 -0700

    extract asset paths

--- src/config/gro.config.default.ts
+++ src/config/gro.config.default.ts
@@ -5,12 +5,13 @@ import {GroConfigCreator, PartialGroConfig} from './config.js';
 // This is the default config that's used if the current project does not define one.
 
 const createConfig: GroConfigCreator = async () => {
+	const assetPaths = ['html', 'css', 'ico', 'png', 'jpg', 'webp', 'webm', 'mp3'];
 	const config: PartialGroConfig = {
 		builds: [
 			{
 				name: 'browser',
 				platform: 'browser',
-				input: ['index.ts', (id) => id.endsWith('.html') || id.endsWith('.css')],
+				input: ['index.ts', createFilter(`**/*.{${assetPaths.join(',')}}`)],
 				dist: true,
 			},
 			{
