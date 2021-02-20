317f9409844e5d56dff3317284823b77de14126c
Ryan Atkinson <ryanatkn@gmail.com>
2021-02-19 18:45:46 -0700

    add simple test component

--- src/frontend/App.svelte
+++ src/frontend/App.svelte
@@ -1,9 +1,35 @@
 <script>
+	import {fade} from 'svelte/transition';
+
+	import FilerVisualizer from './FilerVisualizer.svelte';
+
 	export let name;
 	console.log('enter App.svelte');
+
+	let showFilerVisualizer1 = false;
+	let showFilerVisualizer2 = false;
 </script>
 
 <main>name: {name}</main>
+
+<button on:click={() => (showFilerVisualizer1 = !showFilerVisualizer1)}>{#if showFilerVisualizer1}
+		hide filer visualizer 1
+	{:else}show filer visualizer 1{/if}</button>
+
+<button on:click={() => (showFilerVisualizer2 = !showFilerVisualizer2)}>{#if showFilerVisualizer2}
+		hide filer visualizer 2
+	{:else}show filer visualizer 2{/if}</button>
+
+{#if showFilerVisualizer2}
+	<div in:fade>
+		<FilerVisualizer />
+	</div>
+{/if}
+{#if showFilerVisualizer1}
+	<div in:fade>
+		<FilerVisualizer />
+	</div>
+{/if}
 
 <style>
 	main {
---
+++ src/frontend/FilerVisualizer.svelte
@@ -0,0 +1,1 @@
+<h3>Filer Visualizer</h3>
