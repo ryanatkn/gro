a7786007fad54f6530bc524928ca5a045f59d936
Ryan Atkinson <ryanatkn@gmail.com>
2021-03-11 20:58:09 -0700

    extract path helpers and fix rendering build ids

--- src/client/BuildId.svelte
+++ src/client/BuildId.svelte
@@ -1,20 +1,11 @@
 <script>
 	import {getContext} from 'svelte';
 
+	import {toRootPath} from './pathHelpers.js';
+
 	export let id;
 
 	const ctx = getContext('buildContext');
-
-	// TODO refactor, move where?
-	const toRootPath = (id, buildDir) => {
-		let start = buildDir.length - 1;
-		for (let i = buildDir.length - 2; i >= 0; i--) {
-			const char = buildDir[i];
-			if (char === '/') break;
-			start--;
-		}
-		return id.slice(start - 1);
-	};
 
 	$: displayed = id.startsWith($ctx.buildDir) ? toRootPath(id, $ctx.buildDir) : id;
 </script>
