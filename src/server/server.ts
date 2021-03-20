ccbe6ae83674ba67c6fd6847bbc7adc39dcdcb7a
Ryan Atkinson <mail@ryanatkn.com>
2021-03-19 20:43:42 -0600

    working but copypasta

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,15 +2,14 @@ import {
 	createServer as createHttp1Server,
 	Server as Http1Server,
 	RequestListener as Http1RequestListener,
-	IncomingMessage as Http1ServerRequest,
 	ServerResponse as Http1ServerResponse,
 	OutgoingHttpHeaders,
 } from 'http';
 import {
 	createSecureServer as createHttp2Server,
 	Http2Server,
-	Http2ServerRequest,
-	Http2ServerResponse,
+	IncomingHttpHeaders,
+	ServerHttp2Stream,
 } from 'http2';
 import {ListenOptions} from 'net';
 
@@ -30,6 +29,12 @@ import {paths} from '../paths.js';
 import {loadPackageJson} from '../project/packageJson.js';
 import {ProjectState} from './projectState.js';
 
+type Http2StreamHandler = (
+	stream: ServerHttp2Stream,
+	headers: IncomingHttpHeaders,
+	flags: number,
+) => void;
+
 export interface DevServer {
 	readonly server: Http1Server | Http2Server;
 	start(): Promise<void>;
@@ -44,7 +49,7 @@ export interface Options {
 	filer: Filer;
 	host: string;
 	port: number;
-	https: {cert: string; key: string} | null;
+	https: {cert: string; key: string; allowHTTP1?: boolean} | null;
 	log: Logger;
 }
 export type RequiredOptions = 'filer';
@@ -83,10 +88,9 @@ export const createDevServer = (opts: InitialOptions): DevServer => {
 	};
 	let server: Http1Server | Http2Server;
 	if (https) {
-		server = createHttp2Server(https, createHttp1RequestListener(filer, log) as any);
-		// TODO streaming!
-		// server.on('error', (err) => log.error(err));
-		// server.on('stream', createHttp2StreamHandler(filer, log));
+		server = createHttp2Server(https);
+		server.on('error', (err) => log.error(err));
+		server.on('stream', createHttp2StreamListener(filer, log));
 	} else {
 		server = createHttp1Server(createHttp1RequestListener(filer, log));
 	}
@@ -132,6 +136,66 @@ export const createDevServer = (opts: InitialOptions): DevServer => {
 	return devServer;
 };
 
+// TODO refactor with `createHttp1RequestListener`, this is a lot of copypasta
+const createHttp2StreamListener = (filer: Filer, log: Logger): Http2StreamHandler => {
+	return async (stream, headers) => {
+		const rawUrl = headers[':path'];
+		if (!rawUrl) return stream.end();
+		const url = parseUrl(rawUrl);
+		const localPath = toLocalPath(url);
+		log.trace('serving', gray(rawUrl), '→', gray(localPath));
+
+		// TODO refactor - see `./projectState.ts` for more
+		// can we get a virtual source file with an etag? (might need to sort files if they're not stable?)
+		// also, `src/` is hardcoded below in `paths.source`s
+		const SOURCE_ROOT_MATCHER = /^\/src\/?$/;
+		if (SOURCE_ROOT_MATCHER.test(url)) {
+			const projectState: ProjectState = {
+				buildDir: filer.buildDir,
+				sourceDir: paths.source,
+				items: Array.from(filer.sourceMetaById.values()),
+				buildConfigs: filer.buildConfigs!,
+				packageJson: await loadPackageJson(),
+			};
+			stream.respond({
+				':status': 200,
+				'Content-Type': 'application/json',
+			});
+			return stream.end(JSON.stringify(projectState));
+		}
+
+		// search for a file with this path
+		let file = await filer.findByPath(localPath);
+		if (!file) {
+			// TODO this is just temporary - the more correct code is below. The filer needs to support directories.
+			file = await filer.findByPath(`${localPath}/index.html`);
+		}
+		// if (file?.type === 'directory') { // or `file?.isDirectory`
+		// 	file = filer.findById(file.id + '/index.html');
+		// }
+		if (!file) {
+			log.info(`${yellow('404')} ${red(localPath)}`);
+			stream.respond({
+				':status': 404,
+				'Content-Type': 'text/plain; charset=utf-8',
+			});
+			return stream.end(`404 not found: ${url}`);
+		}
+		// console.log('req headers', gray(file.id), headers);
+		const etag = headers['if-none-match'];
+		if (etag && etag === toETag(file)) {
+			log.info(`${yellow('304')} ${gray(localPath)}`);
+			stream.respond({':status': 304});
+			return stream.end();
+		}
+		log.info(`${yellow('200')} ${gray(localPath)}`);
+		const responseHeaders = await to200Headers(file);
+		responseHeaders[':status'] = 200;
+		stream.respond(responseHeaders);
+		return stream.end(getFileContentsBuffer(file));
+	};
+};
+
 const createHttp1RequestListener = (filer: Filer, log: Logger): Http1RequestListener => {
 	const requestListener: Http1RequestListener = async (req, res) => {
 		if (!req.url) return;
@@ -170,7 +234,7 @@ const createHttp1RequestListener = (filer: Filer, log: Logger): Http1RequestList
 		// }
 		if (!file) {
 			log.info(`${yellow('404')} ${red(localPath)}`);
-			return send404(req, res);
+			return send404(res, url);
 		}
 		// console.log('req headers', gray(file.id), req.headers);
 		const etag = req.headers['if-none-match'];
@@ -179,7 +243,7 @@ const createHttp1RequestListener = (filer: Filer, log: Logger): Http1RequestList
 			return send304(res);
 		}
 		log.info(`${yellow('200')} ${gray(localPath)}`);
-		return send200(req, res, file);
+		return send200(res, file);
 	};
 	return requestListener;
 };
@@ -194,27 +258,26 @@ const toLocalPath = (url: string): string => {
 	return relativePath;
 };
 
-const send404 = (
-	req: Http1ServerRequest | Http2ServerRequest,
-	res: Http1ServerResponse | Http2ServerResponse,
-) => {
+const send404 = (res: Http1ServerResponse, url: string) => {
 	const headers: OutgoingHttpHeaders = {
 		'Content-Type': 'text/plain; charset=utf-8',
 	};
 	res.writeHead(404, headers);
-	res.end(`404 not found: ${req.url}`);
+	res.end(`404 not found: ${url}`);
 };
 
-const send304 = (res: Http1ServerResponse | Http2ServerResponse) => {
+const send304 = (res: Http1ServerResponse) => {
 	res.writeHead(304);
 	res.end();
 };
 
-const send200 = async (
-	_req: Http1ServerRequest | Http2ServerRequest,
-	res: Http1ServerResponse | Http2ServerResponse,
-	file: BaseFilerFile,
-) => {
+const send200 = async (res: Http1ServerResponse, file: BaseFilerFile) => {
+	const headers = await to200Headers(file);
+	res.writeHead(200, headers);
+	res.end(getFileContentsBuffer(file));
+};
+
+const to200Headers = async (file: BaseFilerFile): Promise<OutgoingHttpHeaders> => {
 	const stats = await getFileStats(file);
 	const mimeType = getFileMimeType(file);
 	const headers: OutgoingHttpHeaders = {
@@ -238,8 +301,7 @@ const send200 = async (
 		// 'Cache-Control': 'max-age=31536000',
 	};
 	// console.log('res headers', gray(file.id), headers);
-	res.writeHead(200, headers);
-	res.end(getFileContentsBuffer(file));
+	return headers;
 };
 
 const toETag = (file: BaseFilerFile): string => `"${getFileContentsHash(file)}"`;
