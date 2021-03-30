05e72725b6952d2a424294b3688ea017f3378660
Ryan Atkinson <mail@ryanatkn.com>
2021-03-30 17:07:25 -0600

    export async helpers from root

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ export {TaskError} from './task/task.js';
 
 export type {Gen} from './gen/gen.js';
 
+// by definition, these are generic, so just export everything
 export * from './utils/types.js';
+
+// these seem useful and generic enough to
+export type {AsyncStatus} from './utils/async.js';
+export {wait, wrap} from './utils/async.js';
