e45e7443b60178fd2a00cf37491875f99a86087a
Ryan Atkinson <mail@ryanatkn.com>
2021-03-30 18:20:14 -0600

    detect deprecated frontend project form

--- src/config/gro.config.default.ts
+++ src/config/gro.config.default.ts
@@ -3,13 +3,14 @@ import {createFilter} from '@rollup/pluginutils';
 import type {GroConfigCreator, PartialGroConfig} from './config.js';
 import {LogLevel} from '../utils/log.js';
 import {PartialBuildConfig} from './buildConfig.js';
+import {pathExists} from '../fs/nodeFs.js';
 
 // This is the default config that's used if the current project does not define one.
 
 const createConfig: GroConfigCreator = async () => {
 	const config: PartialGroConfig = {
 		builds: [
-			toDefaultBrowserBuild(),
+			(await hasDeprecatedGroFrontend()) ? toDefaultBrowserBuild() : null,
 			{
 				name: 'node',
 				platform: 'node',
@@ -24,9 +25,20 @@ const createConfig: GroConfigCreator = async () => {
 export default createConfig;
 
 const assetPaths = ['html', 'css', 'json', 'ico', 'png', 'jpg', 'webp', 'webm', 'mp3'];
+
 const toDefaultBrowserBuild = (): PartialBuildConfig => ({
 	name: 'browser',
 	platform: 'browser',
 	input: ['index.ts', createFilter(`**/*.{${assetPaths.join(',')}}`)],
 	dist: true,
 });
+
+// TODO extract helper?
+const hasDeprecatedGroFrontend = async (): Promise<boolean> => {
+	// TODO these paths
+	const [hasIndexHtml, hasIndexTs] = await Promise.all([
+		pathExists('src/index.html'),
+		pathExists('src/index.ts'),
+	]);
+	return hasIndexHtml && hasIndexTs;
+};
