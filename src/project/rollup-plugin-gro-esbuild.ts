7163daa584b40c266a33d8af725415a802c69b7f
Ryan Atkinson <mail@ryanatkn.com>
2021-04-02 22:11:22 -0600

    fix rollup plugin return values

--- src/project/rollup-plugin-gro-esbuild.ts
+++ src/project/rollup-plugin-gro-esbuild.ts
@@ -85,8 +85,9 @@ export const groEsbuildPlugin = (opts: InitialOptions): Plugin => {
 				},
 			};
 			onstats(id, stats, handleStats, this, log);
-
-			return output;
+			// TODO onwarn
+			// output.warnings;
+			return {code: output.code, map: esbuildOptions.sourcemap ? output.map : null};
 		},
 	};
 };
