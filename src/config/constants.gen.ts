aa1d9cbf7c9ab5537dfd26b745048549f707310d
Ryan Atkinson <mail@ryanatkn.com>
2021-04-05 17:42:51 -0600

    add a note

--- src/config/constants.gen.ts
+++ src/config/constants.gen.ts
@@ -17,6 +17,8 @@ export const gen: Gen = async ({originId}) => {
 	return renderTsHeaderAndFooter(
 		{originId},
 		`
+// note: if bundle size is an issue, prefer this module to using /src/fs/mime.ts when possible
+
 // these can be mutated at init
 export const DEFAULT_ASSET_PATHS: string[] = [${Array.from(getExtensions())
 			.map((e) => `'${e}'`)
