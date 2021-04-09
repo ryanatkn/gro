78795254865f8a2fec784f06eb24d60a8f43f096
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-04-09 10:17:50 -0500

    fix tests when none are present and check before version (#167)

--- src/test.task.ts
+++ src/test.task.ts
@@ -5,6 +5,9 @@ import {Timings} from './utils/time.js';
 import {spawnProcess} from './utils/process.js';
 import {toBuildOutPath, toRootPath} from './paths.js';
 import {PRIMARY_NODE_BUILD_CONFIG_NAME} from './config/defaultBuildConfig.js';
+import {pathExists} from './fs/node.js';
+import {loadGroConfig} from './config/config.js';
+import {buildSourceDirectory} from './build/buildSourceDirectory.js';
 
 // Runs the project's tests: `gro test [...args]`
 // Args are passed through directly to `uvu`'s CLI:
@@ -17,12 +20,31 @@ export const task: Task = {
 	run: async ({dev, log, args}): Promise<void> => {
 		const timings = new Timings();
 
-		const dir = toRootPath(toBuildOutPath(dev, PRIMARY_NODE_BUILD_CONFIG_NAME));
+		const testsBuildDir = toBuildOutPath(dev, PRIMARY_NODE_BUILD_CONFIG_NAME);
+
+		// TODO cleaner way to detect & rebuild?
+		if (!(await pathExists(testsBuildDir))) {
+			const timingToLoadConfig = timings.start('load config');
+			const config = await loadGroConfig(dev);
+			timingToLoadConfig();
+
+			const timingToPrebuild = timings.start('prebuild');
+			await buildSourceDirectory(config, dev, log);
+			timingToPrebuild();
+
+			// Projects may not define any artifacts for the Node build,
+			// and we don't force anything out in that case,
+			// so just exit early if that happens.
+			if (!(await pathExists(testsBuildDir))) {
+				log.info('no tests found');
+				return;
+			}
+		}
 
 		const timeToRunUvu = timings.start('run test with uvu');
 		const testRunResult = await spawnProcess('npx', [
 			'uvu',
-			dir,
+			toRootPath(testsBuildDir),
 			...(args._.length ? args._ : DEFAULT_TEST_FILE_PATTERNS),
 			...process.argv.slice(3),
 		]);
