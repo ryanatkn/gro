44b69fc42e6a426afbe44a4731e6bdc8051d8b9e
Ryan Atkinson <mail@ryanatkn.com>
2021-05-11 00:16:04 -0600

    implement default adapt

--- src/config/defaultAdapt.ts
+++ src/config/defaultAdapt.ts
@@ -1,40 +1,19 @@
-import type {AdaptBuilds} from './adapt';
+import type {AdaptBuilds} from './adapt.js';
+import {hasDeprecatedGroFrontend} from './defaultBuildConfig.js';
 
 // TODO copy dist ? autodetect behavior?
 
-export const defaultAdapt: AdaptBuilds = async () => {
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
-	console.log('adapt!!');
+export const defaultAdapt: AdaptBuilds = async ({fs}) => {
+	const [enableDeprecatedGroFrontend] = await Promise.all([
+		// enableApiServer,
+		// hasApiServer(fs),
+		hasDeprecatedGroFrontend(fs),
+	]);
 	return [
-		{
-			name: 'default-adapter',
-			adapt: async (ctx) => {
-				console.log('adapting!', ctx);
-			},
-		},
+		enableDeprecatedGroFrontend
+			? (await import('./gro-adapter-bundled-frontend.js')).createAdapter()
+			: null,
+		// TODO
+		// (await import('./gro-adapter-node-lib.js')).createAdapter(),
 	];
 };
