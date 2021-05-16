811359e6ce065bf9bdcc696bb919cbd9e9d0140d
Ryan Atkinson <mail@ryanatkn.com>
2021-05-16 11:26:09 -0600

    do not define for gro outputs

--- src/build/esbuildBuildHelpers.ts
+++ src/build/esbuildBuildHelpers.ts
@@ -2,6 +2,7 @@ import type esbuild from 'esbuild';
 import type * as sveltePreprocessEsbuild from 'svelte-preprocess-esbuild';
 
 import {DEFAULT_ECMA_SCRIPT_TARGET} from '../build/defaultBuildConfig.js';
+import {isThisProjectGro} from '../paths.js';
 import type {EcmaScriptTarget} from './tsBuildHelpers.js';
 
 export interface EsbuildTransformOptions extends esbuild.TransformOptions {
@@ -21,7 +22,11 @@ export const getDefaultEsbuildOptions = (
 	charset: 'utf8', // following `svelte-preprocess-esbuild` here
 	tsconfigRaw: {compilerOptions: {importsNotUsedAsValues: 'remove'}},
 	// TODO hacky but trying to get dev build and publishing stuff figured out
-	define: dev ? undefined : {'process.env.NODE_ENV': dev ? '"development"' : '"production"'},
+	// the more correct way is probably making a `define` option for user configs
+	define:
+		dev || isThisProjectGro
+			? undefined
+			: {'process.env.NODE_ENV': dev ? '"development"' : '"production"'},
 });
 
 export const getDefaultEsbuildPreprocessOptions = (
@@ -33,5 +38,9 @@ export const getDefaultEsbuildPreprocessOptions = (
 	sourcemap,
 	tsconfigRaw: {compilerOptions: {}}, // pass an empty object so the preprocessor doesn't load the tsconfig
 	// TODO hacky but trying to get dev build and publishing stuff figured out
-	define: dev ? undefined : {'process.env.NODE_ENV': dev ? '"development"' : '"production"'},
+	// the more correct way is probably making a `define` option for user configs
+	define:
+		dev || isThisProjectGro
+			? undefined
+			: {'process.env.NODE_ENV': dev ? '"development"' : '"production"'},
 });
