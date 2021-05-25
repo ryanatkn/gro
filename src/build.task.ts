831acafcfe3d4492784957ae78b45ea421209ecb
Ryan Atkinson <mail@ryanatkn.com>
2021-05-25 09:36:39 -0600

    wip

--- src/build.task.ts
+++ src/build.task.ts
@@ -37,7 +37,7 @@ export const task: Task<TaskArgs, TaskEvents> = {
 
 		const timings = new Timings(); // TODO belongs in ctx
 
-		await clean(fs, {dist: true}, log);
+		await clean(fs, {dist: true, buildProd: true}, log);
 
 		// Build all types so they're available.
 		// TODO refactor? maybe lazily build types only when a builder wants them
