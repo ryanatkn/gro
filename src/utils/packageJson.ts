98e1274f86da2adec5bc8acf248afd8b9123c1e8
Ryan Atkinson <mail@ryanatkn.com>
2021-05-28 18:42:30 -0600

    typo

--- src/utils/packageJson.ts
+++ src/utils/packageJson.ts
@@ -1,5 +1,5 @@
 import {join} from 'path';
-import type {Json} from '@feltcoopp/felt/utils/json.js';
+import type {Json} from '@feltcoop/felt/utils/json.js';
 
 import type {Filesystem} from '../fs/filesystem.js';
 import {paths, groPaths, isThisProjectGro} from '../paths.js';
