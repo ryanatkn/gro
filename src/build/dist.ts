2519b77e7a30678bff0a3085eeaf01bc554fb29c
Ryan Atkinson <mail@ryanatkn.com>
2021-06-20 21:42:38 -0500

    hack for types

--- src/build/dist.ts
+++ src/build/dist.ts
@@ -1,6 +1,6 @@
 import {relative, dirname} from 'path';
 import type {Logger} from '@feltcoop/felt/util/log.js';
-import {strip_end} from '@feltcoop/felt/util/string.js';
+import {strip_end, strip_start} from '@feltcoop/felt/util/string.js';
 
 import type {Build_Config} from '../build/build_config.js';
 import type {Filesystem} from '../fs/filesystem.js';
@@ -18,8 +18,8 @@ import {
 	to_types_build_dir,
 } from '../paths.js';
 
-// TODO make typemaps optional - how? on the `Build_Config`?
-// or as an arg? on the main Gro config?
+// TODO this has a huge hack to copy all types over to the dist because
+// we're not including `import type` imports as dependencies yet
 
 export const copy_dist = async (
 	fs: Filesystem,
@@ -33,37 +33,39 @@ export const copy_dist = async (
 	const build_out_dir = to_build_out_path(dev, build_config.name);
 	const externals_dir = to_build_out_path(dev, build_config.name, EXTERNALS_BUILD_DIRNAME);
 	log.info(`copying ${print_path(build_out_dir)} to ${print_path(dist_out_dir)}`);
-	let has_types = false;
-	const typemap_files: Set<string> = new Set(); // TODO convert to array when type hacks below are fixed
+	const copied_type_files: Set<string> = new Set(); // TODO HACK -- see above, delete this
+	const typemap_files: string[] = [];
 	await fs.copy(build_out_dir, dist_out_dir, {
 		filter: async (id) => {
 			if (id === externals_dir) return false;
 			const stats = await fs.stat(id);
 			if (filter && !filter(id, stats)) return false;
 			if (stats.isDirectory()) return true;
+			// TODO HACK -- see above, delete this
+			if (id.endsWith(TS_TYPE_EXTENSION) || id.endsWith(TS_TYPEMAP_EXTENSION)) {
+				copied_type_files.add(id);
+			}
 			// typemaps are edited before copying, see below
 			if (id.endsWith(TS_TYPEMAP_EXTENSION)) {
-				typemap_files.add(id);
+				typemap_files.push(id);
 				return false;
-			}
-			// TODO HACK for types -- see comment below
-			if (!has_types) {
-				if (id.endsWith(TS_TYPE_EXTENSION) || id.endsWith(TS_TYPEMAP_EXTENSION)) {
-					has_types = true;
-				}
 			}
 			return true;
 		},
 	});
 
-	// TODO HACK for types -- include all type files because
-	// we're not including `import type` imports as dependencies yet
-	if (has_types) {
+	// TODO HACK -- see above, delete this
+	const hack_typemap_files: string[] = [];
+	if (copied_type_files.size) {
 		await fs.copy(to_types_build_dir(), dist_out_dir, {
 			filter: async (id) => {
-				const should_copy = !typemap_files.has(id);
+				const stats = await fs.stat(id);
+				if (filter && !filter(id, stats)) return false;
+				if (stats.isDirectory()) return true;
+				const should_copy = !copied_type_files.has(id);
 				if (should_copy && id.endsWith(TS_TYPEMAP_EXTENSION)) {
-					typemap_files.add(id);
+					hack_typemap_files.push(id);
+					return false;
 				}
 				return should_copy;
 			},
@@ -75,6 +77,22 @@ export const copy_dist = async (
 	await Promise.all(
 		Array.from(typemap_files).map(async (id) => {
 			const base_path = to_build_base_path(id);
+			const source_base_path = `${strip_end(base_path, TS_TYPEMAP_EXTENSION)}${TS_EXTENSION}`;
+			const dist_source_id = pack
+				? `${dist_out_dir}/${SOURCE_DIRNAME}/${source_base_path}`
+				: `${paths.source}${source_base_path}`;
+			const dist_out_path = `${dist_out_dir}/${base_path}`;
+			const typemap_source_path = relative(dirname(dist_out_path), dist_source_id);
+			const typemap = JSON.parse(await fs.read_file(id, 'utf8'));
+			typemap.sources[0] = typemap_source_path; // haven't seen any exceptions that would break this
+			return fs.write_file(dist_out_path, JSON.stringify(typemap));
+		}),
+	);
+
+	// TODO HACK -- see above, delete this
+	await Promise.all(
+		Array.from(hack_typemap_files).map(async (id) => {
+			const base_path = strip_start(id, to_types_build_dir()).substring(1);
 			const source_base_path = `${strip_end(base_path, TS_TYPEMAP_EXTENSION)}${TS_EXTENSION}`;
 			const dist_source_id = pack
 				? `${dist_out_dir}/${SOURCE_DIRNAME}/${source_base_path}`
