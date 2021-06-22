8fb4dfa69a1e1eee5a6d21d9556bcb47d5cfcfd0
Ryan Atkinson <mail@ryanatkn.com>
2021-06-22 12:36:01 -0500

    wip

--- src/client/devtools.ts
+++ src/client/devtools.ts
@@ -1,4 +1,7 @@
 import {Gro_Devtools} from './Gro_Devtools.js';
+import {test_absolute_import} from '$lib/test_absolute_import.js';
+
+console.log('devtools test_absolute_import', test_absolute_import);
 
 declare global {
 	namespace globalThis {
