d1a32b8e53ef4afbec90df9e82de4061e165134b
Ryan Atkinson <mail@ryanatkn.com>
2021-06-23 09:58:37 -0500

    add tests

---
+++ src/client/test_imports/test_imports.ts
@@ -0,0 +1,7 @@
+// test lib imports
+import {test_lib_import} from '$lib/test_lib_import.js';
+console.log('test_nested_imports test_lib_import', test_lib_import);
+
+// test absolute imports
+import {test_absolute_import} from 'src/lib/test_absolute_import.js';
+console.log('test_nested_imports test_absolute_import', test_absolute_import);
