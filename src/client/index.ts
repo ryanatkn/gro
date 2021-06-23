441e209ef277a9d7bc0e51baed01f28fbf8de17f
Ryan Atkinson <mail@ryanatkn.com>
2021-06-23 09:50:14 -0500

    update import test

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,13 +1,6 @@
 import './devtools.js';
 import App from './App.svelte';
-
-// test lib imports
-import {test_lib_import} from '$lib/test_lib_import.js';
-console.log('devtools test_lib_import', test_lib_import);
-
-// test absolute imports
-import {test_absolute_import} from 'src/lib/test_absolute_import.js';
-console.log('devtools test_absolute_import', test_absolute_import);
+import './test_imports/test_imports.js';
 
 const root = document.getElementById('root');
 if (!root) throw Error('Cannot find root element');
