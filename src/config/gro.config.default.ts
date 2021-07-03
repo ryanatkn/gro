f02d32c550cac6f864b56d83dbc0a118ea6265c9
Ryan Atkinson <mail@ryanatkn.com>
2021-07-03 10:35:17 -0600

    update library detection

--- src/config/gro.config.default.ts
+++ src/config/gro.config.default.ts
@@ -17,10 +17,10 @@ It looks at the project and tries to do the right thing:
 
 - if `src/routes` and `src/app.html`,
 	assumes a SvelteKit frontend
+- if `src/lib/index.ts`,
+	assumes a Node library
 - if `src/index.html` and `src/index.ts`,
 	assumes Gro's deprecated SPA mode - https://github.com/feltcoop/gro/issues/106
-- if `src/index.ts` and not `src/index.html`,
-	assumes a Node library
 - if `src/server/server.ts`,
 	assumes a Node API server
 
