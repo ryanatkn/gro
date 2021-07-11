666d0e69a3be1ab16645cf55428de120fc769489
Ryan Atkinson <mail@ryanatkn.com>
2021-07-11 00:25:52 -0500

    remove an unused import

--- src/build/Filer.ts
+++ src/build/Filer.ts
@@ -16,7 +16,7 @@ import {create_filer_dir} from '../build/filer_dir.js';
 import type {Filer_Dir, Filer_Dir_Change_Callback} from '../build/filer_dir.js';
 import {is_input_to_build_config, map_dependency_to_source_id} from './utils.js';
 import type {Map_Dependency_To_Source_Id} from './utils.js';
-import {EXTERNALS_BUILD_DIR_ROOT_PREFIX, JS_EXTENSION, paths, to_build_out_path} from '../paths.js';
+import {JS_EXTENSION, paths, to_build_out_path} from '../paths.js';
 import type {Build, Build_Context, Builder, Builder_State, Build_Result} from './builder.js';
 import {infer_encoding} from '../fs/encoding.js';
 import type {Encoding} from '../fs/encoding.js';
