af79d8664eee9b9971b7a2615dee7f49496832b9
Ryan Atkinson <mail@ryanatkn.com>
2021-07-11 22:22:20 -0500

    export a helper

--- src/build/default_build_config.ts
+++ src/build/default_build_config.ts
@@ -81,7 +81,7 @@ export const to_default_browser_build = (
 });
 
 // compute default asset extensions on demand to pick up any changes to the supported MIME types
-const to_default_asset_extensions = (): string[] =>
+export const to_default_asset_extensions = (): string[] =>
 	Array.from(get_extensions()).filter(
 		(extension) => !default_non_asset_extensions.has(`.${extension}`),
 	);
