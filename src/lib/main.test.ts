3b14539259a6de6bf82ad328064e245b0f7ad0eb
Ryan Atkinson <mail@ryanatkn.com>
2021-07-26 01:18:52 -0500

    rearrange for clarity

--- src/lib/main.test.ts
+++ src/lib/main.test.ts
@@ -1,10 +1,10 @@
+// Gro creates this file to help you with sourcemaps and other global test setup and teardown.
+// To customize the file's location,
+// use the Gro config option `main_test: 'lib/main.test.ts'`,
+// or turn it off with `null`.
+
 import sourcemap_support from 'source-map-support';
 
 sourcemap_support.install({
 	handleUncaughtExceptions: false,
 });
-
-// Gro creates this file to help you with sourcemaps and other global test setup and teardown.
-// To customize the file's location,
-// use the Gro config option `main_test: 'lib/main.test.ts'`,
-// or turn it off with `null`.
