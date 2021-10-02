b6d3b0cb8e0e2f09984dad1613d3805015a187c9
Ryan Atkinson <mail@ryanatkn.com>
2021-10-02 02:22:53 -0500

    try paths.source

--- src/typecheck.task.ts
+++ src/typecheck.task.ts
@@ -1,7 +1,7 @@
 import {printSpawnResult, spawn} from '@feltcoop/felt/util/process.js';
 
 import type {Task} from 'src/task/task.js';
-import {SOURCE_DIRNAME} from './paths.js';
+import {paths} from './paths.js';
 import {TaskError} from './task/task.js';
 
 export const task: Task = {
@@ -11,7 +11,7 @@ export const task: Task = {
 		if (!tscTypecheckResult.ok) {
 			throw new TaskError(`Failed to typecheck. ${printSpawnResult(tscTypecheckResult)}`);
 		}
-		const svelteCheckResult = await spawn('npx', ['svelte-check', '--workspace', SOURCE_DIRNAME]);
+		const svelteCheckResult = await spawn('npx', ['svelte-check', '--workspace', paths.source]);
 		if (!svelteCheckResult.ok) {
 			throw new TaskError(`Failed to typecheck Svelte. ${printSpawnResult(svelteCheckResult)}`);
 		}
