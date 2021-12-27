ff03bed44fc82ef6fada83810518c90980f7988e
Ryan Atkinson <mail@ryanatkn.com>
2021-12-27 12:22:00 -0600

    set importsNotUsedAsValues to remove

--- src/build/buildDependency.test.ts
+++ src/build/buildDependency.test.ts
@@ -1,7 +1,7 @@
 import {suite} from 'uvu';
 import * as assert from 'uvu/assert';
 
-import type {BuildDependency, SerializedBuildDependency} from 'src/build/buildDependency.js';
+import {type BuildDependency, type SerializedBuildDependency} from './buildDependency.js';
 import {serializeBuildDependency, deserializeBuildDependency} from './buildDependency.js';
 
 /* test__serializeBuildDependency */
