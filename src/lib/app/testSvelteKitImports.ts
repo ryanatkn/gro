a8d1fda39fb8696248acbc101c4393dbf119d783
Ryan Atkinson <mail@ryanatkn.com>
2022-01-06 22:23:27 -0600

    add mocks

--- src/lib/app/testSvelteKitImports.ts
+++ src/lib/app/testSvelteKitImports.ts
@@ -1,4 +1,4 @@
-import {} from '$app/env';
-export {goto} from '$app/navigation';
-import {} from '$app/paths';
-import {} from '$app/stores';
+export {amp, browser, dev, mode, prerendering} from '$app/env';
+export {goto, invalidate, prefetch, prefetchRoutes} from '$app/navigation';
+export {assets, base} from '$app/paths';
+export {getStores, navigating, page, session} from '$app/stores';
