113c7712c8453312cefd7b0742d5c10efeacb1cb
Ryan Atkinson <mail@ryanatkn.com>
2022-01-14 10:36:59 -0600

    try with dir

--- src/adapt/groAdapterNodeLibrary.ts
+++ src/adapt/groAdapterNodeLibrary.ts
@@ -96,7 +96,7 @@ export const createAdapter = ({
 					mapBundleOptions({
 						entryPoints: input,
 						bundle: true,
-						outfile: 'TODO', // `outputDir`
+						outfile: outputDir,
 					}),
 				);
 				timingToBundle();
