ea6f98babf564e622eea1bd38df653aa30d96ea0
Ryan Atkinson <mail@ryanatkn.com>
2022-01-14 09:55:10 -0600

    fix linking

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,6 +2,23 @@ import CheapWatch from 'cheap-watch';
 import {resolve, join} from 'path';
 import esbuild from 'esbuild';
 import fs from 'fs-extra';
+import {spawn} from 'child_process';
+
+/*
+
+This file was previous a one-liner npm script,
+but changing use TypeScript 4.5's `type` import modifier
+causes esbuild to fail with `preserveValueInputs: true`,
+which is different than the TypeScript's compiler behavior.
+
+Not sure if I'm doing something wrong, or if it's a bug in esbuild.
+Seems likely it's the former. But as far as I can tell,
+its behavior isn't matching what `tsc` outputs.
+
+So we have this rather complex file to bootstrap the project.
+See this issue for more: https://github.com/feltcoop/gro/pull/292
+
+*/
 
 const transformOptions = {
 	target: 'es2020',
@@ -12,7 +29,6 @@ const transformOptions = {
 	tsconfigRaw: {
 		compilerOptions: {
 			importsNotUsedAsValues: 'remove',
-			// TODO why does this need to be disabled? behaves differently than `tsc`?
 			// preserveValueImports: true,
 		},
 	},
@@ -20,10 +36,12 @@ const transformOptions = {
 
 const bootstrap = async () => {
 	const dir = resolve('src');
-	const outDir = resolve('dist');
+	const distDir = './dist';
+	const outDir = resolve(distDir);
 	await fs.remove(outDir);
 	const watcher = new CheapWatch({
 		dir,
+		// @ts-ignore
 		filter: ({path, stats}) => stats.isDirectory() || path.endsWith('.ts'),
 		watch: false,
 	});
@@ -38,6 +56,7 @@ const bootstrap = async () => {
 			if (stats.isDirectory()) return;
 			count++;
 			const contents = await fs.readFile(join(dir, path), 'utf8');
+			// @ts-ignore
 			const transformed = esbuild.transformSync(contents, transformOptions);
 			const outPath = join(outDir, path).slice(0, -2) + 'js';
 			await fs.outputFile(outPath, transformed.code);
@@ -45,6 +64,22 @@ const bootstrap = async () => {
 	);
 
 	console.log(`transformed ${count} files in ${Date.now() - startTime}ms`);
+
+	// @ts-ignore
+	let done, promise, ps;
+	promise = new Promise((r) => (done = r));
+	ps = spawn('chmod', ['+x', distDir + '/cli/gro.js']);
+	ps.on('error', (err) => console.error('err', err));
+	// @ts-ignore
+	ps.on('close', () => done());
+	await promise;
+
+	promise = new Promise((r) => (done = r));
+	ps = spawn('npm', ['link']);
+	ps.on('error', (err) => console.error('err', err));
+	// @ts-ignore
+	ps.on('close', () => done());
+	await promise;
 };
 
 bootstrap().catch((err) => {
