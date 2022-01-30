30dcbb4803e9b0cd1c6a9a5048e231fb58fd4a2b
Ryan Atkinson <mail@ryanatkn.com>
2022-01-30 09:29:05 -0600

    add a log line

--- src/lint.task.ts
+++ src/lint.task.ts
@@ -5,12 +5,14 @@ import {SOURCE_DIRNAME} from './paths.js';
 
 export const task: Task = {
 	summary: 'run eslint on the source files',
-	run: async ({fs}): Promise<void> => {
+	run: async ({fs, log}): Promise<void> => {
 		if (await fs.exists('node_modules/.bin/eslint')) {
 			const eslintResult = await spawn('npx', ['eslint', SOURCE_DIRNAME]);
 			if (!eslintResult.ok) {
 				throw new TaskError(`ESLint found some problems. ${printSpawnResult(eslintResult)}`);
 			}
+		} else {
+			log.info('ESLint is not installed; skipping linting');
 		}
 	},
 };
