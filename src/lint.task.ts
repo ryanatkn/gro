04e1b9b793b219c916162a1a1e36b5a85d4146f1
Ryan Atkinson <mail@ryanatkn.com>
2022-01-30 09:27:05 -0600

    add lint task

---
+++ src/lint.task.ts
@@ -0,0 +1,16 @@
+import {printSpawnResult, spawn} from '@feltcoop/felt/util/process.js';
+
+import {TaskError, type Task} from './task/task.js';
+import {SOURCE_DIRNAME} from './paths.js';
+
+export const task: Task = {
+	summary: 'run eslint on the source files',
+	run: async ({fs}): Promise<void> => {
+		if (await fs.exists('node_modules/.bin/eslint')) {
+			const eslintResult = await spawn('npx', ['eslint', SOURCE_DIRNAME]);
+			if (!eslintResult.ok) {
+				throw new TaskError(`ESLint found some problems. ${printSpawnResult(eslintResult)}`);
+			}
+		}
+	},
+};
