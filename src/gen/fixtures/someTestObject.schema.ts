9a43119eda6cfa5c6d6fc831f9d71838dc400d76
Ryan Atkinson <mail@ryanatkn.com>
2022-02-01 23:32:36 -0600

    wip

--- src/gen/fixtures/someTestObject.schema.ts
+++ src/gen/fixtures/someTestObject.schema.ts
@@ -11,10 +11,13 @@ export const SomeTestObjectSchema = {
 		},
 		d: {
 			type: 'object',
-			tsType: 'A<B>',
+			tsType: 'A<B<C<D<typeof E, C2, typeof F, typeof E2, typeof F2>>>>',
 			tsImport: [
 				` import {type A} from "./someTestTypes.js" `, // no semicolon, double quotes, whitespace
-				`import {type B} from "./someTestTypes.js"`,
+				`import {type B, type C} from "./someTestTypes.js"`,
+				` import  E ,  {type D,type C as C2,  type  F} from "./someTestTypes.js"`,
+				`import E2 from "./someTestTypes.js"`,
+				`import type {F as F2} from "./someTestTypes.js"`,
 			],
 		},
 	},
