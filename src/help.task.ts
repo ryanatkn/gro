0ed0cc8183b44e24e0799121769ec88d4e7038a6
Ryan Atkinson <mail@ryanatkn.com>
2022-02-07 18:52:50 -0600

    rework help

--- src/help.task.ts
+++ src/help.task.ts
@@ -3,8 +3,8 @@ import {spawn} from '@feltcoop/felt/util/process.js';
 import {type Task} from './task/task.js';
 
 export const task: Task = {
-	summary: 'alias for `gro --help`',
+	summary: 'alias for `gro` with no task name provided',
 	run: async (): Promise<void> => {
-		await spawn('npx', ['gro', '--help']);
+		await spawn('npx', ['gro']);
 	},
 };
