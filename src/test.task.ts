c18fbbd06ede62034daa905442a134c10f2ab4fe
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2022-04-26 22:35:50 -0500

    always build before running tasks (#322)

--- src/test.task.ts
+++ src/test.task.ts
@@ -6,8 +6,6 @@ import {yellow} from 'kleur/colors';
 import {TaskError, type Task} from './task/task.js';
 import {toBuildOutPath, toRootPath} from './paths.js';
 import {SYSTEM_BUILD_NAME} from './build/buildConfigDefaults.js';
-import {loadConfig} from './config/config.js';
-import {buildSource} from './build/buildSource.js';
 import type {TestTaskArgs} from './testTask.js';
 import {TestTaskArgsSchema} from './testTask.schema.js';
 import {addArg, printCommandArgs, serializeArgs, toForwardedArgs} from './utils/args.js';
@@ -25,15 +23,6 @@ export const task: Task<TestTaskArgs> = {
 		const {_: testFilePatterns} = args;
 
 		const timings = new Timings();
-
-		const timingToLoadConfig = timings.start('load config');
-		const config = await loadConfig(fs, dev);
-		timingToLoadConfig();
-
-		// TODO cleaner way to detect & rebuild?
-		const timingToPrebuild = timings.start('prebuild');
-		await buildSource(fs, config, dev, log);
-		timingToPrebuild();
 
 		// Projects may not define any artifacts for the Node build,
 		// and we don't force anything out in that case,
