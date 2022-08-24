002a2a4cd59b4349e0422570ab1dcbb3aa561a88
Ryan Atkinson <mail@ryanatkn.com>
2022-08-23 21:24:22 -0500

    add excludes to upgrade task

--- src/upgradeTask.schema.ts
+++ src/upgradeTask.schema.ts
@@ -4,7 +4,14 @@ export const UpgradeTaskArgsSchema: ArgsSchema = {
 	$id: '/schemas/UpgradeTaskArgs.json',
 	type: 'object',
 	properties: {
+		_: {
+			type: 'array',
+			items: {type: 'string'},
+			default: [],
+			description: 'names of deps to exclude from the upgrade',
+		},
 		dry: {type: 'boolean', default: false, description: 'if true, print out the planned upgrades'},
 	},
+	required: ['_'],
 	additionalProperties: false,
 };
