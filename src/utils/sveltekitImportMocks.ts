e126711eb39a42655e33f5a161d8a6c1c0344e63
Ryan Atkinson <mail@ryanatkn.com>
2022-09-06 14:17:02 -0500

    finish mocks

--- src/utils/sveltekitImportMocks.ts
+++ src/utils/sveltekitImportMocks.ts
@@ -1,22 +1,39 @@
-import {readable} from 'svelte/store';
+import type {Navigation} from '@sveltejs/kit';
+import {readable, type Readable} from 'svelte/store';
 
 // see this issue: https://github.com/sveltejs/kit/issues/1485
 
-export const amp = false;
+// $app/environment
 export const browser = false;
 export const dev = true;
-export const mode = 'development';
 export const prerendering = false;
 
+// $app/paths
 export const assets = ''; // TODO read from Svelte config
 export const base = ''; // TODO read from Svelte config
 
+// $app/navigation
+export const afterNavigate: (callback: (navigation: Navigation) => void) => void = (_cb) => {}; // eslint-disable-line @typescript-eslint/no-empty-function
+export const beforeNavigate: (
+	callback: (
+		navigation: Navigation & {
+			cancel: () => void;
+		},
+	) => void,
+) => void = (_cb) => {}; // eslint-disable-line @typescript-eslint/no-empty-function
+export const disableScrollHandling: () => void = () => {}; // eslint-disable-line @typescript-eslint/no-empty-function
 export const goto = (): Promise<void> => Promise.resolve();
 export const invalidate = (_href: string): Promise<object> => Promise.resolve({});
+export const invalidateAll = (): Promise<void> => Promise.resolve();
 export const prefetch = (_href: string): Promise<object> => Promise.resolve({});
 export const prefetchRoutes = (_routes: string[] | undefined): Promise<object> =>
 	Promise.resolve({});
 
+// $app/stores
 export const navigating = readable(null);
 export const page = readable({host: '', path: '', params: new URLSearchParams(), query: {}});
-export const getStores = () => ({navigating, page} as const);
+export const updated: Readable<boolean> & {
+	check: () => boolean;
+} = readable(true) as any;
+updated.check = () => true;
+export const getStores = () => ({navigating, page, updated} as const);
