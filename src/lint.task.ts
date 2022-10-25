5b9fddc2a9a325aeb6799a80023396e4e625cfaf
Ryan Atkinson <mail@ryanatkn.com>
2022-10-25 10:34:52 -0500

    use zod in lint.task.ts (#343)

--- src/lint.task.ts
+++ src/lint.task.ts
@@ -1,13 +1,20 @@
 import {printSpawnResult, spawn} from '@feltcoop/felt/util/process.js';
+import {z} from 'zod';
 
 import {TaskError, type Task} from './task/task.js';
-import {printCommandArgs, serializeArgs, toForwardedArgs} from './utils/args.js';
-import type {LintTaskArgs} from './lintTask';
-import {LintTaskArgsSchema} from './lintTask.schema.js';
+import {printCommandArgs, serializeArgs, toForwardedArgs, type ArgsSchema} from './utils/args.js';
+import {SOURCE_DIRNAME} from './paths.js';
+import {toVocabSchema} from './utils/schema.js';
 
-export const task: Task<LintTaskArgs> = {
+const Args = z.object({
+	_: z.array(z.string(), {description: 'paths to serve'}).default([SOURCE_DIRNAME]),
+});
+type Args = z.infer<typeof Args>;
+
+export const task: Task<Args> = {
 	summary: 'run eslint on the source files',
-	args: LintTaskArgsSchema,
+	Args,
+	args: toVocabSchema(Args, 'LintTaskArgs') as ArgsSchema,
 	run: async ({fs, log, args}): Promise<void> => {
 		if (!(await fs.exists('node_modules/.bin/eslint'))) {
 			log.info('ESLint is not installed; skipping linting');
