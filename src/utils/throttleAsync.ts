0dff977ea3ed8bd3ce074ea7c21d427e3141ba4d
Ryan Atkinson <mail@ryanatkn.com>
2022-11-09 17:32:26 -0600

    fixes

--- src/utils/throttleAsync.ts
+++ src/utils/throttleAsync.ts
@@ -1,4 +1,4 @@
-import {wait} from '@feltcoop/felt';
+import {wait} from '@feltcoop/felt/util/async.js';
 
 // TODO maybe support return values? gets tricky: what should it return for the skipped ones?
 
