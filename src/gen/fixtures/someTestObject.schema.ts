53112fed3d353a2c3ff063a8640105ff1ef830f9
Ryan Atkinson <mail@ryanatkn.com>
2023-05-25 23:08:19 -0600

    wip

--- src/gen/fixtures/someTestObject.schema.ts
+++ src/gen/fixtures/someTestObject.schema.ts
@@ -35,7 +35,7 @@ export const SomeTestObjectSchema: VocabSchema = {
 	additionalProperties: false,
 };
 
-export const SomeTestPrimitiveSchema = {
+export const SomeTestPrimitiveSchema: VocabSchema = {
 	$id: '/schemas/SomeTestPrimitive',
 	type: 'number',
 };
