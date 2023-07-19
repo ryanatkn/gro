e068c56008889827335bbde724c5a4db35f4cb86
Ryan Atkinson <mail@ryanatkn.com>
2023-07-18 21:15:49 -0600

    "ma"

--- src/commit.task.ts
+++ src/commit.task.ts
@@ -1,8 +1,8 @@
 import {spawn} from '@feltjs/util/process.js';
 import {z} from 'zod';
-import {exec} from 'child_process';
+import {execSync} from 'child_process';
 
-import {TaskError, type Task} from './task/task.js';
+import type {Task} from './task/task';
 
 const Args = z
 	.object({
@@ -21,11 +21,8 @@ export const task: Task<Args> = {
 		console.log(`args`, args);
 		const {_: rest} = args;
 
-		exec('git rev-parse --abbrev-ref HEAD', async (err, stdout, stderr) => {
-			console.log(`stdout`, stdout);
-			if (err) throw new TaskError('error: ' + stderr);
-			await spawn('git', ['commit', '-am', ...rest]);
-			await spawn('git', ['push', '-u', 'origin', stdout]);
-		});
+		const branch = execSync('git rev-parse --abbrev-ref HEAD').toString();
+		await spawn('git', ['commit', '-am', `"${rest.join(' ')}"`]);
+		await spawn('git', ['push', '-u', 'origin', branch]);
 	},
 };
