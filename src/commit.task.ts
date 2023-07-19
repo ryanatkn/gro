edf19b0c9b9a36396c8a7895077ca4825a7796e0
Ryan Atkinson <mail@ryanatkn.com>
2023-07-18 20:55:20 -0600

    wip

--- src/commit.task.ts
+++ src/commit.task.ts
@@ -1,28 +1,20 @@
-import {join} from 'path';
 import {spawn} from '@feltjs/util/process.js';
-import {printError} from '@feltjs/util/print.js';
-import {green, red} from 'kleur/colors';
 import {z} from 'zod';
+import {exec} from 'child_process';
 
-import {rainbow} from './utils/colors.js';
-import type {Task} from './task/task.js';
-import {DIST_DIR, GIT_DIRNAME, paths, printPath, SVELTEKIT_DIST_DIRNAME} from './paths.js';
-import {cleanFs} from './fs/clean.js';
-import {toRawRestArgs} from './utils/args.js';
-import {GIT_DEPLOY_SOURCE_BRANCH, GIT_DEPLOY_TARGET_BRANCH} from './build/buildConfigDefaults.js';
+import {TaskError, type Task} from './task/task.js';
+import {paths} from './paths.js';
 
 // TODO customize
 const WORKTREE_DIRNAME = 'worktree';
 const WORKTREE_DIR = `${paths.root}${WORKTREE_DIRNAME}`;
-const ORIGIN = 'origin';
-const INITIAL_FILE = 'package.json'; // this is a single file that's copied into the new branch to bootstrap it
-const TEMP_PREFIX = '__TEMP__';
 const GIT_ARGS = {cwd: WORKTREE_DIR};
-const DANGEROUS_BRANCHES = ['main', 'master'];
 
 const Args = z
 	.object({
-		_: z.array(z.string()),
+		_: z.array(
+			z.string({description: 'the git commit message, the same as git commit -m or --message'}),
+		),
 	})
 	.strict();
 type Args = z.infer<typeof Args>;
@@ -31,9 +23,15 @@ export const task: Task<Args> = {
 	summary: 'commit and push to a new branch',
 	production: true,
 	Args,
-	run: async ({fs, args, log}): Promise<void> => {
+	run: async ({args}): Promise<void> => {
 		console.log(`args`, args);
+		const {_: rest} = args;
 
-		await spawn('git', ['commit', '-m', 'commitment'], GIT_ARGS);
+		exec('git rev-parse --abbrev-ref HEAD', async (err, stdout, stderr) => {
+			console.log(`stdout`, stdout);
+			if (err) throw new TaskError('error: ' + stderr);
+			await spawn('git', ['commit', '-am', ...rest], GIT_ARGS);
+			await spawn('git', ['push', '-u', 'origin', stdout], GIT_ARGS);
+		});
 	},
 };
