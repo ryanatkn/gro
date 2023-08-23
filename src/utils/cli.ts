27497de71393a8095db41be88e965ed185fd418b
Ryan Atkinson <mail@ryanatkn.com>
2023-08-23 10:19:36 -0600

    add cli helper

---
+++ src/utils/cli.ts
@@ -0,0 +1,35 @@
+import {execSync, type SpawnOptions} from 'node:child_process';
+import {spawn, type SpawnResult} from '@feltjs/util/process.js';
+
+import type {Filesystem} from '../fs/filesystem.js';
+
+/**
+ * Looks for the CLI `name`, first local to the cwd and then globally.
+ */
+export const findCli = async (fs: Filesystem, name: string): Promise<'local' | 'global' | null> => {
+	if (await fs.exists(`node_modules/.bin/${name}`)) {
+		return 'local';
+	}
+	try {
+		execSync('command -v changeset > /dev/null 2>&1');
+		return 'global';
+	} catch (err) {
+		return null;
+	}
+};
+
+/**
+ * Calls the CLI `name` if available, first local to the cwd and then globally.
+ */
+export const execCli = async (
+	fs: Filesystem,
+	name: string,
+	args: any[] = [],
+	options?: SpawnOptions | undefined,
+): Promise<SpawnResult | undefined> => {
+	const found = await findCli(fs, name);
+	if (!found) return;
+	const command = found === 'local' ? 'npx' : name;
+	const finalArgs = found === 'local' ? [name].concat(args) : args;
+	return spawn(command, finalArgs, options);
+};
