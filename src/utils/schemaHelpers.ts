fb6c4212c4ff02ccc23c66fb3cc4dc8a1922a77e
Ryan Atkinson <mail@ryanatkn.com>
2023-08-25 09:06:28 -0600

    wip

--- src/utils/schemaHelpers.ts
+++ src/utils/schemaHelpers.ts
@@ -3,9 +3,13 @@ import {zodToJsonSchema} from 'zod-to-json-schema';
 
 import type {JsonSchema} from './schema.js';
 
-export const toJsonSchema = (t: z.ZodType<any, z.ZodTypeDef, any>, name: string): JsonSchema => {
+export const toJsonSchema = (
+	t: z.ZodType<any, z.ZodTypeDef, any>,
+	name: string,
+	prefix = '/schemas/',
+): JsonSchema => {
 	const schema = zodToJsonSchema(t, name);
 	const args = (schema.definitions ? schema.definitions[name] : {}) as JsonSchema;
-	args.$id = `/schemas/${name}`;
+	args.$id = prefix + name;
 	return args;
 };
