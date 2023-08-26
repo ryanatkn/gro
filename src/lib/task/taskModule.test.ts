4182724666f6d2bf42cc68272989733df1ccca12
Ryan Atkinson <mail@ryanatkn.com>
2023-08-26 11:41:19 -0600

    wip

--- src/lib/task/taskModule.test.ts
+++ src/lib/task/taskModule.test.ts
@@ -25,7 +25,7 @@ const test__loadTaskModule = suite('loadTaskModule');
 
 test__loadTaskModule('basic behavior', async () => {
 	const name = 'task/fixtures/testTaskModule.taskFixture.js';
-	const id = resolve('src/' + name);
+	const id = resolve('src/lib/' + name);
 	const result = await loadTaskModule(id, true);
 	assert.ok(result.ok);
 	assert.is(result.mod.id, id);
@@ -66,7 +66,7 @@ test__loadTaskModule.run();
 const test__loadTaskModules = suite('loadTaskModules');
 
 test__loadTaskModules('basic behavior', async () => {
-	const result = await loadTaskModules(fs, [resolve('src/test'), resolve('src/test.task.ts')]);
+	const result = await loadTaskModules(fs, [resolve('src/lib/test'), resolve('src/lib/test.task.ts')]);
 	assert.ok(result.ok);
 	assert.is(result.modules.length, 1);
 	assert.is(result.modules[0].mod, actualTestTaskModule);
