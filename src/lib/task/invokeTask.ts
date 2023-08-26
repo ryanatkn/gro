4795c58d7ca8d7159abb52ddaa88cc165c6beacd
Ryan Atkinson <mail@ryanatkn.com>
2023-08-25 21:33:44 -0600

    wip

--- src/lib/task/invokeTask.ts
+++ src/lib/task/invokeTask.ts
@@ -28,7 +28,7 @@ import {logAvailableTasks, logErrorReasons} from './logTask.js';
  * Invokes Gro tasks by name using the filesystem as the source.
  *
  * When a task is invoked,
- * it first searches for tasks in the current working directory.
+ * Gro first searches for tasks in the current working directory.
  * and falls back to searching Gro's directory, if the two are different.
  * See `src/lib/fs/inputPath.ts` for info about what "taskName" can refer to.
  * If it matches a directory, all of the tasks within it are logged,
