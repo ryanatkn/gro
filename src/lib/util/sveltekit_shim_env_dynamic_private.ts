cf9926615cd90ca5efde4fd6a5dd02173b17f69a
Ryan Atkinson <mail@ryanatkn.com>
2023-09-02 13:25:47 -0600

    wip

--- src/lib/util/sveltekit_shim_env_dynamic_private.ts
+++ src/lib/util/sveltekit_shim_env_dynamic_private.ts
@@ -2,11 +2,11 @@
 // @see https://github.com/sveltejs/kit/issues/1485
 
 import {loadEnv} from 'vite';
-import { stripEnd } from '@feltjs/util/string.js';
+import {stripEnd} from '@feltjs/util/string.js';
 
-import { paths } from '../path/paths.js';
+import {paths} from '../path/paths.js';
 
 console.log(`loading paths.root`, stripEnd(paths.root, '/'));
-const env = loadEnv('development', stripEnd(paths.root, '/'));
+const env = loadEnv('development', stripEnd(paths.root, '/'), '');
 
 console.log(`LOADED VITE env`, env);
