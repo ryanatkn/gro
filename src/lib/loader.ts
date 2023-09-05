719f6f535daa573d58a6f4d57ad4ffe6cc10ce93
Ryan Atkinson <mail@ryanatkn.com>
2023-09-04 20:05:00 -0600

    wip

--- src/lib/loader.ts
+++ src/lib/loader.ts
@@ -7,15 +7,10 @@ usage in Gro: node --loader ./dist/loader.js foo.ts
 */
 
 import {transformSync, type TransformOptions} from 'esbuild';
-import {fileURLToPath, pathToFileURL} from 'node:url';
-import {cwd} from 'node:process';
+import {fileURLToPath} from 'node:url';
+import {dirname} from 'path';
 
 import {postprocess} from './build/postprocess.js';
-
-console.log(`cwd()`, cwd());
-console.log(`pathToFileURL(cwd() + '/').href`, pathToFileURL(cwd() + '/').href);
-console.log(`import.meta.url`, import.meta.url);
-console.log(`fileURLToPath(import.meta.url)`, fileURLToPath(import.meta.url));
 
 const transformOptions: TransformOptions = {
 	target: 'esnext',
@@ -34,15 +29,13 @@ const transformOptions: TransformOptions = {
 
 const matcher = /\.(ts|tsx|mts|cts)$/u;
 
-// TODO BLOCK
-const build_dir = '';
-const source_dir = '';
-
 export const load = async (url: string, context: any, nextLoad: any): Promise<any> => {
 	if (matcher.test(url)) {
+		const path = fileURLToPath(url);
+		const dir = dirname(path) + '/';
 		const loaded = await nextLoad(url, {...context, format: 'module'});
 		const transformed = transformSync(loaded.source.toString(), transformOptions);
-		const processed = postprocess(transformed.code, build_dir, source_dir);
+		const processed = postprocess(transformed.code, dir, dir);
 		return {
 			format: 'module',
 			shortCircuit: true,
