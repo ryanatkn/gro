a26d72d5b84bc62f82d52b0acd062ad21ff28597
Ryan Atkinson <mail@ryanatkn.com>
2023-09-11 02:24:11 -0600

    wip

--- src/lib/plugin/gro_plugin_node_server.ts
+++ src/lib/plugin/gro_plugin_node_server.ts
@@ -3,7 +3,6 @@ import {existsSync, readFileSync} from 'node:fs';
 import * as esbuild from 'esbuild';
 import {cwd} from 'node:process';
 import {yellow, red, blue, green} from 'kleur/colors';
-import {dirname, join} from 'node:path';
 import {escapeRegexp} from '@feltjs/util/regexp.js';
 
 import type {Plugin, PluginContext} from './plugin.js';
@@ -143,7 +142,6 @@ export const create_plugin = ({
 				bundle: true,
 				target: config.target,
 				plugins: [
-					// TODO BLOCK extract and refactor with the existing helpers for the loader+postprocess
 					esbuild_plugin_sveltekit_shim_app(),
 					esbuild_plugin_sveltekit_shim_env({
 						dev,
@@ -154,24 +152,18 @@ export const create_plugin = ({
 						ambient_env,
 					}),
 					esbuild_plugin_sveltekit_shim_alias(),
-					esbuild_plugin_sveltekit_local_imports(),
-					// TODO BLOCK a few possibilities here
-					// 1. arrange the plugins in the right order
-					// 2. call resolve from one or the other (or both?)
-					// 3. helpers like `parse_specifier`
+					// TODO BLOCK extract
 					{
 						name: 'external_worker',
 						setup: (build) => {
 							build.onResolve({filter: /\.worker(|\.js|\.ts)$/u}, async ({path, importer}) => {
-								console.log(red('[external_worker] ENTER'), yellow(path), '\n', importer);
-
-								const absolute_path = join(dirname(importer), path);
-								console.log(`absolute_path`, absolute_path);
+								const parsed = await parse_specifier(path, importer);
+								console.log(red('[external_worker] ENTER'), yellow(path), '\n', importer, parsed);
+								const {final_path, source_path, namespace} = parsed;
 
 								// TODO BLOCK make sure this isn't called more than once if 2 files import it (probably need to cache)
 								const build_result = await esbuild.build({
-									// TODO BLOCK refactor options with above
-									entryPoints: [absolute_path],
+									entryPoints: [source_path],
 									outdir,
 									outbase: paths.lib, // TODO configure
 									format: 'esm',
@@ -195,10 +187,11 @@ export const create_plugin = ({
 								});
 								print_build_result(log, build_result);
 
-								return {path, external: true};
+								return {path: final_path, external: true, namespace};
 							});
 						},
 					},
+					esbuild_plugin_sveltekit_local_imports(),
 				],
 			});
 			timing_to_esbuild_create_context();
