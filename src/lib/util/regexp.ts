032522816ca8c4357fcfc4841589ecefc9552e41
Ryan Atkinson <mail@ryanatkn.com>
2023-09-10 19:09:17 -0600

    wip

--- src/lib/util/regexp.ts
+++ src/lib/util/regexp.ts
@@ -1,5 +1,7 @@
 /**
  * Borrowing SvelteKit's homework.
+ * @see https://github.com/sveltejs/kit/blob/master/LICENSE
+ * @license MIT
  */
 export const escape_for_regexp = (str: string): string =>
 	str.replace(/[.*+?^${}()|[\]\\]/gu, (match) => '\\' + match);
