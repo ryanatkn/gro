321f5b24027d1d5fd143936f6ef1a607fd9af8fe
Ryan Atkinson <mail@ryanatkn.com>
2023-09-12 07:08:13 -0600

    wip

--- src/lib/util/hash.test.ts
+++ src/lib/util/hash.test.ts
@@ -1,18 +1,35 @@
 import {suite} from 'uvu';
 import * as assert from 'uvu/assert';
+import {webcrypto} from 'node:crypto';
 
 import {to_hash} from './hash.js';
 
 /* test__to_hash */
 const test__to_hash = suite('to_hash');
 
-test__to_hash('turns a buffer into a string', () => {
-	assert.type(to_hash(Buffer.from('hey')), 'string');
+test__to_hash('turns a buffer into a string', async () => {
+	assert.type(await to_hash(Buffer.from('hey')), 'string');
 });
 
-test__to_hash('returns the same value given the same input', () => {
-	assert.is(to_hash(Buffer.from('hey')), to_hash(Buffer.from('hey')));
+test__to_hash('returns the same value given the same input', async () => {
+	assert.is(await to_hash(Buffer.from('hey')), await to_hash(Buffer.from('hey')));
+});
+
+test__to_hash('checks against an implementation copied from MDN', async () => {
+	const data = Buffer.from('some_test_string');
+	assert.is(await to_hash_from_mdn_example(data), await to_hash(data));
 });
 
 test__to_hash.run();
 /* test__to_hash */
+
+/**
+ * Copied from https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+ * and compared against our implementation for extra assurances, because cryptography.
+ */
+const to_hash_from_mdn_example = async (data: Buffer): Promise<string> => {
+	const digested = await webcrypto.subtle.digest('SHA-256', data);
+	return Array.from(new Uint8Array(digested))
+		.map((h) => h.toString(16).padStart(2, '0'))
+		.join('');
+};
--- src/lib/util/hash.ts
+++ src/lib/util/hash.ts
@@ -1,6 +1,18 @@
-import {createHash} from 'node:crypto';
+import {webcrypto} from 'node:crypto';
 
-// Note that this uses md5 and therefore is not cryptographically secure.
-// It's fine for now, but some use cases may need security.
-export const to_hash = (buf: Buffer): string =>
-	createHash('md5').update(buf).digest().toString('hex');
+const {subtle} = webcrypto;
+
+// TODO add a helper that converts from a string to call `to_hash` using browser APIs
+
+export const to_hash = async (
+	data: Buffer,
+	algorithm: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512' = 'SHA-256',
+): Promise<string> => {
+	const digested = await subtle.digest(algorithm, data);
+	const bytes = Array.from(new Uint8Array(digested));
+	let hex = '';
+	for (const h of bytes) {
+		hex += h.toString(16).padStart(2, '0');
+	}
+	return hex;
+};
