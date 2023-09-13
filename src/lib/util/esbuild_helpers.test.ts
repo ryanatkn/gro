1f93802e938b9a902d5b67b15c105327e3667cb1
Ryan Atkinson <mail@ryanatkn.com>
2023-09-12 20:44:27 -0600

    wip

--- src/lib/util/esbuild_helpers.test.ts
+++ src/lib/util/esbuild_helpers.test.ts
@@ -13,8 +13,6 @@ const dir = paths.lib + 'util/fixtures/';
 test__parse_specifier('parses a ts specifier', async () => {
 	assert.equal(await parse_specifier(dir + 'test_ts.ts', dir + 'importer.ts', dir), {
 		specifier: './test_ts.js',
-		// TODO BLOCK remove probably
-		// specifier_id: dir + 'test_ts.js',
 		source_id: dir + 'test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -23,7 +21,6 @@ test__parse_specifier('parses a ts specifier', async () => {
 test__parse_specifier('parses an extensionless specifier', async () => {
 	assert.equal(await parse_specifier(dir + 'test_ts', dir + 'importer.ts', dir), {
 		specifier: './test_ts.js',
-		// specifier_id: dir + 'test_ts.js',
 		source_id: dir + 'test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -32,7 +29,6 @@ test__parse_specifier('parses an extensionless specifier', async () => {
 test__parse_specifier('parses a js specifier', async () => {
 	assert.equal(await parse_specifier(dir + 'test_js.js', dir + 'importer.ts', dir), {
 		specifier: './test_js.js',
-		// specifier_id: dir + 'test_js.js',
 		source_id: dir + 'test_js.js',
 		namespace: 'sveltekit_local_imports_js',
 	});
@@ -41,7 +37,6 @@ test__parse_specifier('parses a js specifier', async () => {
 test__parse_specifier('parses a js specifier as ts for a file that does not exist', async () => {
 	assert.equal(await parse_specifier(dir + 'test_missing.js', dir + 'importer.ts', dir), {
 		specifier: './test_missing.js',
-		// specifier_id: dir + 'test_missing.js',
 		source_id: dir + 'test_missing.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -52,7 +47,6 @@ test__parse_specifier(
 	async () => {
 		assert.equal(await parse_specifier(dir + 'test_missing', dir + 'importer.ts', dir), {
 			specifier: './test_missing.js',
-			// specifier_id: dir + 'test_missing.js',
 			source_id: dir + 'test_missing.ts',
 			namespace: 'sveltekit_local_imports_ts',
 		});
@@ -62,7 +56,6 @@ test__parse_specifier(
 test__parse_specifier('parses from a directory 1 deeper', async () => {
 	assert.equal(await parse_specifier(dir + 'test_ts.ts', dir + '/a/importer.ts', dir), {
 		specifier: '../test_ts.js',
-		// specifier_id: dir + 'test_ts.js',
 		source_id: dir + 'test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -71,7 +64,6 @@ test__parse_specifier('parses from a directory 1 deeper', async () => {
 test__parse_specifier('parses from a directory 2 deeper', async () => {
 	assert.equal(await parse_specifier(dir + 'test_ts.ts', dir + '/a/b/importer.ts', dir), {
 		specifier: '../../test_ts.js',
-		// specifier_id: dir + 'test_ts.js',
 		source_id: dir + 'test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -80,7 +72,6 @@ test__parse_specifier('parses from a directory 2 deeper', async () => {
 test__parse_specifier('parses from a directory 1 shallower', async () => {
 	assert.equal(await parse_specifier(dir + 'test_ts.ts', join(dir + '../importer.ts'), dir), {
 		specifier: './fixtures/test_ts.js',
-		// specifier_id: dir + 'test_ts.js',
 		source_id: dir + 'test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -89,7 +80,6 @@ test__parse_specifier('parses from a directory 1 shallower', async () => {
 test__parse_specifier('parses from a directory 2 shallower', async () => {
 	assert.equal(await parse_specifier(dir + 'test_ts.ts', join(dir + '../../importer.ts'), dir), {
 		specifier: './util/fixtures/test_ts.js',
-		// specifier_id: dir + 'test_ts.js',
 		source_id: dir + 'test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -98,7 +88,6 @@ test__parse_specifier('parses from a directory 2 shallower', async () => {
 test__parse_specifier('parses a relative ts specifier', async () => {
 	assert.equal(await parse_specifier('./test_ts.ts', dir + 'importer.ts', dir), {
 		specifier: './test_ts.js',
-		// specifier_id: dir + 'test_ts.js',
 		source_id: dir + 'test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -107,7 +96,6 @@ test__parse_specifier('parses a relative ts specifier', async () => {
 test__parse_specifier('parses a relative ts specifier 2 deeper', async () => {
 	assert.equal(await parse_specifier('./a/b/test_ts.ts', dir + 'importer.ts', dir), {
 		specifier: './a/b/test_ts.js',
-		// specifier_id: dir + 'a/b/test_ts.js',
 		source_id: dir + 'a/b/test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -116,7 +104,6 @@ test__parse_specifier('parses a relative ts specifier 2 deeper', async () => {
 test__parse_specifier('parses a relative ts specifier 2 shallower', async () => {
 	assert.equal(await parse_specifier('../../test_ts.ts', dir + 'importer.ts', dir), {
 		specifier: '../../test_ts.js',
-		// specifier_id: join(dir, '../../test_ts.js'),
 		source_id: join(dir, '../../test_ts.ts'),
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -125,7 +112,6 @@ test__parse_specifier('parses a relative ts specifier 2 shallower', async () => 
 test__parse_specifier('parses a ts specifier with a relative importer', async () => {
 	assert.equal(await parse_specifier(dir + 'test_ts.ts', './importer.ts', dir), {
 		specifier: './test_ts.js',
-		// specifier_id: dir + 'test_ts.js',
 		source_id: dir + 'test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -134,7 +120,6 @@ test__parse_specifier('parses a ts specifier with a relative importer', async ()
 test__parse_specifier('parses a ts specifier with a relative importer 2 deeper', async () => {
 	assert.equal(await parse_specifier('./a/b/test_ts.ts', dir + 'importer.ts', dir), {
 		specifier: './a/b/test_ts.js',
-		// specifier_id: dir + 'a/b/test_ts.js',
 		source_id: dir + 'a/b/test_ts.ts',
 		namespace: 'sveltekit_local_imports_ts',
 	});
@@ -143,7 +128,6 @@ test__parse_specifier('parses a ts specifier with a relative importer 2 deeper',
 test__parse_specifier('parses a ts specifier with a relative importer 2 shallower', async () => {
 	assert.equal(await parse_specifier('../../test_ts.ts', dir + 'importer.ts', dir), {
 		specifier: '../../test_ts.js',
-		// specifier_id: join(dir, '../../test_ts.js'),
 		source_id: join(dir, '../../test_ts.ts'),
 		namespace: 'sveltekit_local_imports_ts',
 	});
