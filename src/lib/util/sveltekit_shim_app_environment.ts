3064560cc54128eeaef7cc98e9651093b1a2f89a
Ryan Atkinson <mail@ryanatkn.com>
2023-09-15 20:19:48 -0600

    wip

--- src/lib/util/sveltekit_shim_app_environment.ts
+++ src/lib/util/sveltekit_shim_app_environment.ts
@@ -1,7 +1,9 @@
 // shim for $app/environment
 // @see https://github.com/sveltejs/kit/issues/1485
 
+// TODO BLOCK finish the paths shim first, and probably use the same pattern
+
 export const browser = false;
-export const dev = true; // TODO BLOCK - esm-env? make 2 versions of this module?
-export const building = false; // TODO BLOCK source from SvelteKit somehow?
+export const dev = true;
+export const building = false;
 export const version = '';
