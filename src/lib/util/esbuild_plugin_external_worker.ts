8b5f063bdea9c160df70877b09239b8940080254
Ryan Atkinson <mail@ryanatkn.com>
2023-09-15 20:16:38 -0600

    wip

--- src/lib/util/esbuild_plugin_external_worker.ts
+++ src/lib/util/esbuild_plugin_external_worker.ts
@@ -1,5 +1,4 @@
 import * as esbuild from 'esbuild';
-import {yellow, red, magenta} from 'kleur/colors';
 import type {Logger} from '@feltjs/util/log.js';
 import {basename} from 'node:path';
 import {cwd} from 'node:process';
@@ -49,9 +48,6 @@ export const esbuild_plugin_external_worker = ({
 		const builds: Map<string, Promise<esbuild.BuildResult>> = new Map();
 		const build_worker = async (source_id: string): Promise<esbuild.BuildResult> => {
 			if (builds.has(source_id)) return builds.get(source_id)!;
-			console.log(
-				'------------------------\n------------------------\n------------------------\nBUILDING\n------------------------\n------------------------\n-------------------------',
-			);
 			const building = esbuild.build({
 				entryPoints: [source_id],
 				plugins: [
@@ -75,42 +71,12 @@ export const esbuild_plugin_external_worker = ({
 			return building;
 		};
 
-		build.onResolve(
-			{filter: /\.worker(|\.js|\.ts)$/u},
-			async ({path, importer, resolveDir, ...rest}) => {
-				console.log(magenta('.'.repeat(50) + '\nexternal_worker\n' + '.'.repeat(50)));
-				console.log(
-					red('[external_worker] ENTER'),
-					'\nimporting ' + yellow(path),
-					'\nfrom ' + yellow(importer),
-				);
-				console.log(`rest:`, rest);
-				const parsed = await resolve_specifier(path, importer, resolveDir);
-				console.log(`parsed`, parsed);
-				const {specifier, source_id, namespace} = parsed;
-
-				const build_result = await build_worker(source_id);
-				if (log) print_build_result(log, build_result);
-
-				console.log(`build OPTS build.initialOptions.outdir`, build.initialOptions.outdir);
-				console.log(`build OPTS build.initialOptions.outbase`, build.initialOptions.outbase);
-
-				// TODO what about conflicting file names? need to alias them or something? rare enough to ignore?
-				const final_specifier = './' + basename(specifier);
-				console.log(
-					red('[external_worker] resolved\n'),
-					'\nimporting ' + yellow(final_specifier),
-					'\nfrom ' + yellow(importer),
-					'\nvia ' + yellow(specifier),
-				);
-				// TODO BLOCK
-				// const FINAL_SPEC = await resolve_specifier(
-				// 	specifier_id,
-				// 	build.initialOptions.outdir + '/ignored',
-				// );
-				// console.log(`FINAL_SPEC`, FINAL_SPEC);
-				return {path: final_specifier, external: true, namespace};
-			},
-		);
+		build.onResolve({filter: /\.worker(|\.js|\.ts)$/u}, async ({path, resolveDir}) => {
+			const parsed = await resolve_specifier(path, resolveDir);
+			const {specifier, source_id, namespace} = parsed;
+			const build_result = await build_worker(source_id);
+			if (log) print_build_result(log, build_result);
+			return {path: './' + basename(specifier), external: true, namespace};
+		});
 	},
 });
