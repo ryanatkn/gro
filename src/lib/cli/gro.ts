f299f4b42c91645841bd052a5eaab31c23906340
Ryan Atkinson <mail@ryanatkn.com>
2023-09-17 10:03:16 -0600

    wip

--- src/lib/cli/gro.ts
+++ src/lib/cli/gro.ts
@@ -41,33 +41,35 @@ When using the global CLI, this uses the global Gro installation.
 
 */
 
-const main = async (): Promise<void> => {
-	let path;
+let path;
 
-	const gro_bin_path = resolve(NODE_MODULES_DIRNAME, '.bin/gro');
-	if (await exists(gro_bin_path)) {
-		// case 1
-		// Prefer any locally installed version of Gro.
-		path = join(await realpath(gro_bin_path), '../invoke.js');
+const gro_bin_path = resolve(NODE_MODULES_DIRNAME, '.bin/gro');
+if (await exists(gro_bin_path)) {
+	// case 1
+	// Prefer any locally installed version of Gro.
+	path = join(await realpath(gro_bin_path), '../invoke.js');
+} else {
+	// case 2
+	// If running Gro inside its own repo, require the local dist.
+	// If the local dist is not yet built it will fall back to the global.
+	const file_path = fileURLToPath(import.meta.url);
+	const base_path = 'dist/cli';
+	if ((await exists(`${base_path}/gro.js`)) && (await exists(`${base_path}/invoke.js`))) {
+		path = join(file_path, `../../../${base_path}/invoke.js`);
 	} else {
-		// case 2
-		// If running Gro inside its own repo, require the local dist.
-		// If the local dist is not yet built it will fall back to the global.
-		const file_path = fileURLToPath(import.meta.url);
-		const base_path = 'dist/cli';
-		if ((await exists(`${base_path}/gro.js`)) && (await exists(`${base_path}/invoke.js`))) {
-			path = join(file_path, `../../../${base_path}/invoke.js`);
-		} else {
-			// case 3
-			// Fall back to the version associated with the running CLI.
-			path = join(file_path, '../invoke.js');
-		}
+		// case 3
+		// Fall back to the version associated with the running CLI.
+		path = join(file_path, '../invoke.js');
 	}
+}
 
-	await spawn('node', ['--loader', join(path, '../../loader.js'), path, ...process.argv.slice(2)]);
-};
+const result = await spawn('node', [
+	'--loader',
+	join(path, '../../loader.js'),
+	path,
+	...process.argv.slice(2),
+]);
 
-main().catch((err) => {
-	console.error('Gro failed to invoke', err); // eslint-disable-line no-console
-	throw err;
-});
+if (!result.ok) {
+	process.exit(result.code || 1);
+}
