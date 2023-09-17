2d8b2ed81371e8fdb98c0ae756855796232a84a9
Ryan Atkinson <mail@ryanatkn.com>
2023-09-17 17:44:41 -0600

    wip

--- src/lib/task/invoke_task.ts
+++ src/lib/task/invoke_task.ts
@@ -185,18 +185,3 @@ const to_gro_dir_find_modules_result = async (input_path: string, log: Logger) =
 	}
 	return gro_dir_find_modules_result;
 };
-
-/**
- * Maps SvelteKit `$app` specifiers to their Gro shims.
- * @see https://kit.svelte.dev/docs/modules
- */
-export const to_sveltekit_app_specifier = (specifier: string): string | null =>
-	sveltekit_stubbed_specifiers.has(specifier) ? sveltekit_stubbed_specifiers.get(specifier)! : null;
-
-const sveltekit_stubbed_specifiers = new Map([
-	['$app/environment', '@feltjs/gro/util/sveltekit_shim_app_environment.js'],
-	['$app/forms', '@feltjs/gro/util/sveltekit_shim_app_forms.js'],
-	['$app/navigation', '@feltjs/gro/util/sveltekit_shim_app_navigation.js'],
-	['$app/paths', '@feltjs/gro/util/sveltekit_shim_app_paths.js'],
-	['$app/stores', '@feltjs/gro/util/sveltekit_shim_app_stores.js'],
-]);
