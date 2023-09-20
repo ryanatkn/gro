01b677b8129a3e91b039d4f86113a3378857008d
Ryan Atkinson <mail@ryanatkn.com>
2023-09-20 01:48:58 -0600

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import {typescript} from 'svelte-preprocess';
+// @see https://github.com/lukeed/svelte-preprocess-esbuild/issues/8
+//@ts-expect-error
+import {typescript} from 'svelte-preprocess-esbuild';
 import staticAdapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
