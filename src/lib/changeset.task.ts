43332a846ceed6d364b58b68b7822d22ad9e4e17
Ryan Atkinson <mail@ryanatkn.com>
2023-09-20 19:02:43 -0600

    wip

--- src/lib/changeset.task.ts
+++ src/lib/changeset.task.ts
@@ -1,11 +1,16 @@
 import {z} from 'zod';
 import {spawn} from '@feltjs/util/process.js';
+import {red, blue} from 'kleur/colors';
 
 import type {Task} from './task/task.js';
+import {exists} from './util/exists.js';
+
+const RESTRICTED_ACCESS = 'restricted';
 
 export const Args = z
 	.object({
 		_: z.array(z.string(), {description: 'the commands to pass to changeset'}).default([]),
+		access: z.string({description: 'changeset "access" config value'}).default(RESTRICTED_ACCESS),
 	})
 	.strict();
 export type Args = z.infer<typeof Args>;
@@ -15,9 +20,19 @@ export const task: Task<Args> = {
 	Args,
 	run: async (ctx): Promise<void> => {
 		const {
-			args: {_: changet_commands},
+			args: {_: changset_args, access},
+			log,
 		} = ctx;
 
-		await spawn('changeset', changet_commands);
+		const inited = await exists('./.changeset/config.json');
+
+		if (!inited) {
+			await spawn('npx', ['changeset', 'init']);
+
+			const access_color = access === RESTRICTED_ACCESS ? blue : red;
+			log.info('initing changesets with ' + access_color(access) + ' access');
+		}
+
+		await spawn('changeset', changset_args);
 	},
 };
