3fdf188eb886e8837aaccfef69a1df0255711d48
Ryan Atkinson <mail@ryanatkn.com>
2023-09-22 21:29:45 -0600

    wip

---
+++ src/fixtures/some_test_exports.ts
@@ -0,0 +1,22 @@
+export interface A<T = any> {
+	t: T;
+}
+export interface B<T = any> {
+	t: T;
+}
+export interface C<T = any> {
+	t: T;
+}
+export interface D<T1, T2, T3, T4, T5, T6, T7, T8> {
+	t1: T1;
+	t2: T2;
+	t3: T3;
+	t4: T4;
+	t5: T5;
+	t6: T6;
+	t7: T7;
+	t8: T8;
+}
+export const E = {};
+export default E;
+export const F = {};
---
+++ src/fixtures/some_test_exports2.ts
@@ -0,0 +1,2 @@
+export const E3a = {};
+export const E3b = {};
