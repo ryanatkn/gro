5e494ab72eb768c3513145aaa40f8ffdb55a7ea9
Ryan Atkinson <mail@ryanatkn.com>
2023-09-23 20:33:24 -0600

    wip

--- src/lib/gro.config.default.ts
+++ src/lib/gro.config.default.ts
@@ -1,7 +1,7 @@
 import type {GroConfigCreator} from './config.js';
-import {base_path_to_source_id, LIB_DIR, LIB_DIRNAME} from './paths.js';
 import {exists} from './exists.js';
 import {load_package_json} from './package_json.js';
+import {has_server} from './gro_plugin_server.js';
 
 /**
  * This is the default config that's passed to `gro.config.ts`
@@ -21,11 +21,7 @@ const config: GroConfigCreator = async (cfg) => {
 
 	cfg.plugins = async () => [
 		enable_library ? (await import('./gro_plugin_library.js')).plugin() : null,
-		enable_server
-			? (await import('./gro_plugin_server.js')).plugin({
-					entry_points: [SERVER_SOURCE_ID],
-			  })
-			: null,
+		enable_server ? (await import('./gro_plugin_server.js')).plugin() : null,
 		enable_sveltekit_frontend
 			? (await import('./gro_plugin_sveltekit_frontend.js')).plugin({
 					host_target: enable_server ? 'node' : 'github_pages',
@@ -40,10 +36,6 @@ const config: GroConfigCreator = async (cfg) => {
 
 export default config;
 
-export const has_library = async (): Promise<boolean> =>
-	(await exists(LIB_DIR)) && !!(await load_package_json()).exports;
-
-export const has_server = (): Promise<boolean> => exists(SERVER_SOURCE_ID);
-export const SERVER_SOURCE_ID = base_path_to_source_id(LIB_DIRNAME + '/server/server.ts');
+export const has_library = async (): Promise<boolean> => !!(await load_package_json()).exports;
 
 export const has_sveltekit_frontend = (): Promise<boolean> => exists('src/routes');
