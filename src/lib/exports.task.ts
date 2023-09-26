1a19b6358be32211a1829995f1fe5ee270931135
Ryan Atkinson <mail@ryanatkn.com>
2023-09-25 21:55:20 -0600

    wip

--- src/lib/exports.task.ts
+++ src/lib/exports.task.ts
@@ -107,4 +107,4 @@ const create_exports_filter = (include: string, exclude: string) => {
 const failure_message = (when: MapPackageJsonWhen): string =>
 	'Failed exports check.' +
 	` The package.json has unexpectedly changed for \`${when}\`.` +
-	' Run `gro exports` manually to inspect the changes, and check any `package_json` config option.';
+	' Run `gro exports` manually to inspect the changes, and check the `package_json` config option.';
