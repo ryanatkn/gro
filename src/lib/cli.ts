f47b9154f73b91fc7b44d6055d140d67365230c1
Ryan Atkinson <mail@ryanatkn.com>
2023-09-26 13:50:52 -0600

    wip

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,5 +1,5 @@
-import {execSync, type SpawnOptions} from 'node:child_process';
-import {spawn, type SpawnResult} from '@grogarden/util/process.js';
+import type {SpawnOptions} from 'node:child_process';
+import {spawn, spawn_out, type SpawnResult} from '@grogarden/util/process.js';
 import {join} from 'node:path';
 
 import {exists} from './exists.js';
@@ -9,24 +9,11 @@ import {NODE_MODULES_DIRNAME} from './paths.js';
  * Looks for the CLI `name`, first local to the cwd and then globally.
  */
 export const find_cli = async (name: string): Promise<'local' | 'global' | null> => {
-	console.log(`FIND CLI`, name);
 	if (await exists(join(NODE_MODULES_DIRNAME, `.bin/${name}`))) {
-		console.log('found local', name);
 		return 'local';
 	}
-	try {
-		// TODO BLOCK
-		console.log(`FIND GLOBAL CLI!`, name);
-		// execSync(`command -v ${name} > /dev/null 2>&1`);
-		// console.log(`command worked`, name);
-		const spawned = await spawn('command', ['-v name > /dev/null 2>&1']);
-		console.log(`spawned`, spawned);
-		console.log(`FOUND GLOBAL CLI`, name);
-		return 'global';
-	} catch (err) {
-		console.log(`ERR CLI`, name);
-		return null;
-	}
+	const {stdout} = await spawn_out('which', [name]);
+	return stdout === null ? null : 'global';
 };
 
 /**
