c3365acc8a5b075f6530d9298fba337d75d44c7c
Ryan Atkinson <mail@ryanatkn.com>
2023-09-30 01:52:15 -0600

    add release task

--- src/lib/release.task.ts
+++ src/lib/release.task.ts
@@ -1,2 +1,15 @@
-// TODO BLOCK probably this, `gro publish` then `gro deploy`
-// also look into `invoke_task` with a fn
+import {z} from 'zod';
+
+import type {Task} from './task.js';
+
+export const Args = z.object({}).strict();
+export type Args = z.infer<typeof Args>;
+
+export const task: Task<Args> = {
+	summary: 'publish and deploy',
+	Args,
+	run: async ({invoke_task}) => {
+		await invoke_task('publish');
+		await invoke_task('deploy');
+	},
+};
