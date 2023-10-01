88dbe95efdd7baacd5d7cd86df10f982d0de1d4c
Ryan Atkinson <mail@ryanatkn.com>
2023-10-01 11:00:29 -0600

    improve website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,55 @@
 <script lang="ts">
 	import {base} from '$app/paths';
+	import {parse_package_meta} from '@fuz.dev/fuz_library/package.js';
+	import LibraryHeader from '@fuz.dev/fuz_library/LibraryHeader.svelte';
+	import LibraryFooter from '@fuz.dev/fuz_library/LibraryFooter.svelte';
 
 	// TODO add website, rewriting the markdown docs as Svelte
+
+	// TODO SvelteKit warns about this but we put `/static` in `/src` because of what it's saying,
+	/// maybe change to import as the first item from `packages`
+	import package_json from '../static/.well-known/package.json';
+	const pkg = parse_package_meta(package_json.homepage, package_json);
 </script>
 
-<main>
-	<h1>gro</h1>
-	<div class="prose">
-		<div class="box spaced">
-			<blockquote>
-				task runner and toolkit extending <a href="https://kit.svelte.dev/">SvelteKit</a>
-			</blockquote>
-		</div>
-		<div class="box spaced">
-			<code class="padded_md">
-				npm i -D <a href="https://www.npmjs.com/package/@grogarden/gro">@grogarden/gro</a>
-			</code>
-		</div>
-		<div class="panel spaced padded_md text_align_center">
-			this website is a work in progress!<br />for now docs are in
-			<a href="https://github.com/grogarden/gro">the source repo</a>
-		</div>
+<main class="box width_full">
+	<div class="box width_md">
+		<section>
+			<LibraryHeader {pkg} />
+		</section>
+		<section class="panel spaced padded_md text_align_center">
+			this website is a work in progress!<br />
+			<div class="box row spaced">
+				for now, docs are in&nbsp;
+				<a href="https://github.com/grogarden/gro">the source repo</a>
+			</div>
+			<!-- TODO put this in the header, icon support (repo.json?) -->
+			<a class="panel padded_md box" title="source repo" href="https://github.com/grogarden/gro">
+				<img
+					alt="a pixelated green oak acorn with a glint of sun"
+					src="{base}/favicon.png"
+					style:width="var(--icon_size_lg)"
+					style:height="var(--icon_size_lg)"
+				/>
+			</a>
+		</section>
+		<section>
+			<LibraryFooter {pkg} />
+		</section>
 	</div>
 	<div hidden>
 		Mastodon verification:
 		<a rel="me" href="https://hci.social/@ryanatkn">@ryanatkn@hci.social</a>
 	</div>
-	<footer>
-		<a class="panel padded_md box" href="https://github.com/grogarden/gro">
-			<img
-				alt="a pixelated green oak acorn with a glint of sun"
-				class="spaced"
-				src="{base}/favicon.png"
-				style:width="var(--icon_size_lg)"
-				style:height="var(--icon_size_lg)"
-			/>
-			<div>source code</div>
-		</a>
-	</footer>
 </main>
 
 <style>
 	main {
-		margin: 0 auto;
-		max-width: var(--width_md);
-		display: flex;
-		flex-direction: column;
-		align-items: center;
+		margin-bottom: var(--spacing_xl5);
 	}
-	.prose {
-		padding: 20px;
+	section {
+		margin-top: var(--spacing_xl3);
+		margin-bottom: var(--spacing_xl3);
 		display: flex;
 		flex-direction: column;
 		align-items: center;
