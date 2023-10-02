c6b434a2ae82f0824c2f34e66548847d4abe669d
Ryan Atkinson <mail@ryanatkn.com>
2023-10-02 08:54:51 -0600

    fix docs index text

--- src/lib/docs/README.gen.md.ts
+++ src/lib/docs/README.gen.md.ts
@@ -6,12 +6,13 @@ import {type Gen, to_output_file_name} from '../gen.js';
 import {paths, base_path_to_source_id} from '../paths.js';
 import {search_fs} from '../search_fs.js';
 
-// This renders a simple index of a possibly nested directory of files.
-
 // TODO look at `tasks.gen.md.ts` to refactor and generalize
 // TODO show nested structure, not a flat list
 // TODO work with file types beyond markdown
 
+/**
+ * Renders a simple index of a possibly nested directory of files.
+ */
 export const gen: Gen = async ({origin_id}) => {
 	// TODO need to get this from project config or something
 	const root_path = parse_path_segments(paths.root).at(-1);
@@ -40,13 +41,12 @@ export const gen: Gen = async ({origin_id}) => {
 	// TODO do we want to use absolute paths instead of relative paths,
 	// because GitHub works with them and it simplifies the code?
 	const is_index_file = output_file_name === 'README.md';
-	const path_parts = parse_path_parts(relative_dir).map((relative_path_part) =>
-		is_index_file && relative_path_part === relative_dir
-			? relative_path_part
-			: `[${parse_path_segments(relative_path_part).at(-1)}](${
-					relative(origin_dir, base_path_to_source_id(relative_path_part)) || './'
-			  })`,
-	);
+	const path_parts = parse_path_parts(relative_dir).map((relative_path_part) => {
+		const segment = parse_path_segments(relative_path_part).at(-1);
+		return is_index_file && relative_path_part === relative_dir
+			? segment
+			: `[${segment}](${relative(origin_dir, base_path_to_source_id(relative_path_part)) || './'})`;
+	});
 	const breadcrumbs =
 		'> <sub>' + [root_link, ...path_parts, output_file_name].join(' / ') + '</sub>';
 
