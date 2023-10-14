15fb43fc822a34118b6fed9458ca07f0d49b9dd6
Ryan Atkinson <mail@ryanatkn.com>
2023-10-14 12:45:07 -0600

    wip

--- src/lib/format_file.test.ts
+++ src/lib/format_file.test.ts
@@ -10,7 +10,7 @@ test('format_files calls to a function', async () => {
 	assert.is(await format_file(ts_unformatted, {parser: 'typescript'}), ts_formatted);
 
 	const svelte_unformatted = '<style>a{color: red}</style>';
-	const svelte_formatted = '<style>\n\ta{\n\t\tcolor: red;\n\t}\n</style>\n';
+	const svelte_formatted = '<style>\n\ta {\n\t\tcolor: red;\n\t}\n</style>\n';
 	assert.is(await format_file(svelte_unformatted, {filepath: 'foo.svelte'}), svelte_formatted);
 	assert.is(await format_file(svelte_unformatted, {parser: 'svelte'}), svelte_formatted);
 });
