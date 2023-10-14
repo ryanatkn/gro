34a18f5546b9bfbef7d822eaf155f6324ea32b02
Ryan Atkinson <mail@ryanatkn.com>
2023-10-14 12:50:52 -0600

    wip

--- src/lib/format_file.test.ts
+++ src/lib/format_file.test.ts
@@ -3,12 +3,14 @@ import * as assert from 'uvu/assert';
 
 import {format_file} from './format_file.js';
 
-test('format_files calls to a function', async () => {
+test('format ts', async () => {
 	const ts_unformatted = 'hey (1)';
 	const ts_formatted = 'hey(1);\n';
 	assert.is(await format_file(ts_unformatted, {filepath: 'foo.ts'}), ts_formatted);
 	assert.is(await format_file(ts_unformatted, {parser: 'typescript'}), ts_formatted);
+});
 
+test('format svelte', async () => {
 	const svelte_unformatted = '<style>a{color: red}</style>';
 	const svelte_formatted = '<style>\n\ta {\n\t\tcolor: red;\n\t}\n</style>\n';
 	assert.is(await format_file(svelte_unformatted, {filepath: 'foo.svelte'}), svelte_formatted);
