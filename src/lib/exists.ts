6f5248895813a9df2a5fd42294c7985422286116
Ryan Atkinson <mail@ryanatkn.com>
2023-10-30 10:38:24 -0600

    wip

--- src/lib/exists.ts
+++ src/lib/exists.ts
@@ -1,9 +1,11 @@
-import {access} from 'node:fs/promises';
+import {existsSync} from 'node:fs';
 
+// TODO this was using `access` from `node:fs/promises but there's
+// some problem with it and `stat` crashing the process, idk, maybe a quirk of my node version
 export const exists = async (path: string): Promise<boolean> => {
 	try {
-		await access(path);
-		return true;
+		// await access(path);
+		return existsSync(path);
 	} catch (err) {
 		return false;
 	}
