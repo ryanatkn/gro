4e6898fa633dabe46095db07012977025f5e510a
Ryan Atkinson <mail@ryanatkn.com>
2023-12-11 19:59:49 -0700

    wip

--- src/lib/changelog.ts
+++ src/lib/changelog.ts
@@ -1,4 +1,4 @@
-import {readFile} from 'fs/promises';
+import {readFile, writeFile} from 'fs/promises';
 
 /**
  * Updates a changelog produced by `@changesets/changelog-git` with better links and formatting.
@@ -8,6 +8,16 @@ import {readFile} from 'fs/promises';
  */
 export const update_changelog = async (path = 'CHANGELOG.md'): Promise<boolean> => {
 	const contents = await readFile(path, 'utf8');
-	console.log(`contents`, contents);
+	const parsed = parse_changelog(contents);
+	console.log(`parsed`, parsed);
+	// const mapped = map_changelog(parsed);
+	// const updated = serialize_changelog(mapped);
+	// await writeFile(path, updated, 'utf8');
 	return true;
+};
+
+const parse_changelog = (contents: string) => {
+	//
+	const lines = contents.split('\n');
+	console.log(`lines`, lines);
 };
