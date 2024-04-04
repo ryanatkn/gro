f1e11987170311aaa0be9dfd901303eea6319882
Ryan Atkinson <mail@ryanatkn.com>
2024-04-04 08:11:57 -0600

    wip

--- src/lib/package_json.test.ts
+++ src/lib/package_json.test.ts
@@ -57,6 +57,7 @@ test('serialize_package_json fails with bad data', async () => {
 
 test('to_package_exports', async () => {
 	assert.equal(to_package_exports(['a/b.ts']), {
+		'./package.json': './package.json',
 		'./a/b.js': {
 			default: './dist/a/b.js',
 			types: './dist/a/b.d.ts',
@@ -69,6 +70,7 @@ test('to_package_exports', async () => {
 			'a/b/some_test_json.json',
 		]),
 		{
+			'./package.json': './package.json',
 			'./a/b/some_test_json.json': {
 				default: './dist/a/b/some_test_json.json',
 			},
