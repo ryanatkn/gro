0480c63eaa0d2b3a9532b4a1265949b740de4ddf
Ryan Atkinson <mail@ryanatkn.com>
2024-06-14 21:29:14 -0600

    wip

--- src/lib/sveltekit_helpers.ts
+++ src/lib/sveltekit_helpers.ts
@@ -4,6 +4,8 @@ import {sveltekit_config_global} from './sveltekit_config_global.js';
 import type {Parsed_Sveltekit_Config} from './sveltekit_config.js';
 import {SVELTEKIT_CONFIG_FILENAME} from './path_constants.js';
 
+export const has_sveltekit_app = (): Promise<boolean> => exists(SVELTEKIT_CONFIG_FILENAME);
+
 export const has_sveltekit_library = async (
 	package_json?: Package_Json,
 	sveltekit_config: Parsed_Sveltekit_Config = sveltekit_config_global,
@@ -14,5 +16,3 @@ export const has_sveltekit_library = async (
 		(await exists(sveltekit_config.lib_path))
 	);
 };
-
-export const has_sveltekit_app = (): Promise<boolean> => exists(SVELTEKIT_CONFIG_FILENAME);
