b09231cd68a7d4b1a79e392dd7b43b62a6ab673a
Ryan Atkinson <mail@ryanatkn.com>
2024-06-17 15:17:05 -0600

    fix task name inference (#467)

---
+++ .changeset/weak-days-grin.md
@@ -0,0 +1,5 @@
+---
+'@ryanatkn/gro': patch
+---
+
+fix task name inference to node_modules
--- src/lib/task.test.ts
+++ src/lib/task.test.ts
@@ -1,5 +1,6 @@
 import {test} from 'uvu';
 import * as assert from 'uvu/assert';
+import {resolve} from 'node:path';
 
 import {is_task_path, to_task_name} from './task.js';
 
@@ -17,6 +18,19 @@ test('to_task_name basic behavior', () => {
 	assert.is(to_task_name('a/b/c/foo.task.ts', ['a/b/c', 'a']), 'foo');
 	assert.is(to_task_name('a/b/c/foo.task.ts', ['a']), 'b/c/foo');
 	assert.is(to_task_name('a/b/c/foo.task.ts', ['a/b', 'a']), 'c/foo');
+	assert.is(
+		to_task_name(resolve('node_modules/@ryanatkn/gro/dist/foo'), [
+			resolve('./'),
+			resolve('node_modules/@ryanatkn/gro/dist/'),
+		]),
+		'foo',
+		'resolves to node_modules before the cwd',
+	);
+	assert.is(
+		to_task_name(resolve('a/b'), [resolve('b')]),
+		resolve('a/b'),
+		'falls back to the id when unresolved',
+	);
 });
 
 test.run();
