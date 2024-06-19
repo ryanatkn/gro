9ebd833380306809361b3c8ea5ac010b1c3cb441
Ryan Atkinson <mail@ryanatkn.com>
2024-06-18 19:35:03 -0600

    wip

--- src/lib/input_path.test.ts
+++ src/lib/input_path.test.ts
@@ -191,15 +191,21 @@ test('resolve_input_files', async () => {
 		'fake/test3',
 		'fake',
 	]);
-	assert.equal(Array.from(result.resolved_input_files_by_input_path.values()), [
-		[{id: a.id, input_path: a.input_path, resolved_input_path: a}],
-		[{id: b.id, input_path: b.input_path, resolved_input_path: b}],
-		[
-			{id: 'fake/test3/a.ts', input_path: c.input_path, resolved_input_path: c},
-			{id: 'fake/test3/b.ts', input_path: c.input_path, resolved_input_path: c},
-		],
-		[{id: 'fake/test3/c.ts', input_path: e.input_path, resolved_input_path: e}],
-	]);
+	assert.equal(
+		result.resolved_input_files_by_input_path,
+		new Map([
+			['fake/test1.ext.ts', [{id: a.id, input_path: a.input_path, resolved_input_path: a}]],
+			['fake/test2', [{id: b.id, input_path: b.input_path, resolved_input_path: b}]],
+			[
+				'fake/test3',
+				[
+					{id: 'fake/test3/a.ts', input_path: c.input_path, resolved_input_path: c},
+					{id: 'fake/test3/b.ts', input_path: c.input_path, resolved_input_path: c},
+				],
+			],
+			['fake', [{id: 'fake/test3/c.ts', input_path: e.input_path, resolved_input_path: e}]],
+		]),
+	);
 	assert.equal(result.input_directories_with_no_files, [f]);
 });
 
