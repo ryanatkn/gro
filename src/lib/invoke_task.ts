c2e6859dfc6328c2e5f54114d8c1df30ca4ec5dc
Ryan Atkinson <mail@ryanatkn.com>
2024-06-20 17:43:04 -0600

    wip

--- src/lib/invoke_task.ts
+++ src/lib/invoke_task.ts
@@ -11,6 +11,7 @@ import {load_gro_package_json} from './package_json.js';
 import {log_tasks, log_error_reasons} from './task_logging.js';
 import type {Gro_Config} from './config.js';
 
+// TODO BLOCK ignore .git and node_modules OR don't search the fs for the `./` root_dir
 // TODO BLOCK fix/test:
 // from zzz:
 // 		gro ./
