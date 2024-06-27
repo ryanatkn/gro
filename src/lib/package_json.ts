b7217a6bb276be56e27af84a30a3c0da8a67418c
Ryan Atkinson <mail@ryanatkn.com>
2024-06-27 15:49:55 -0600

    wip

--- src/lib/package_json.ts
+++ src/lib/package_json.ts
@@ -146,7 +146,7 @@ export const load_package_json = async (
 	} catch (err) {
 		return EMPTY_PACKAGE_JSON;
 	}
-	parse_or_throw_formatted_error('package.json', Package_Json, package_json);
+	// parse_or_throw_formatted_error('package.json', Package_Json, package_json);
 	if (cache) cache[dir] = package_json;
 	return package_json;
 };
