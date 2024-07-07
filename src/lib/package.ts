4d46d3308de36f301909cb98b7049156094243e9
Ryan Atkinson <mail@ryanatkn.com>
2024-07-06 19:11:41 -0600

    upgrade `@ryanatkn/belt@0.24.4`

--- src/lib/package.ts
+++ src/lib/package.ts
@@ -44,7 +44,7 @@ export const package_json = {
 		'typescript',
 	],
 	dependencies: {
-		'@ryanatkn/belt': '^0.24.3',
+		'@ryanatkn/belt': '^0.24.4',
 		chokidar: '^3.6.0',
 		dotenv: '^16.4.5',
 		'es-module-lexer': '^1.5.4',
