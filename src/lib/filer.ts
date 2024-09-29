695789e5ab09d4734614108dbbf49a6833c85fd3
Ryan Atkinson <mail@ryanatkn.com>
2024-09-29 12:56:16 -0600

    wip

--- src/lib/filer.ts
+++ src/lib/filer.ts
@@ -1,5 +1,12 @@
+import {EMPTY_OBJECT} from '@ryanatkn/belt/object.js';
+
 import type {Path_Id} from './path.js';
-import type {Watch_Node_Fs, Watcher_Change} from './watch_dir.js';
+import {
+	watch_dir,
+	type Watch_Node_Fs,
+	type Watcher_Change,
+	type Options as Watch_Dir_Options,
+} from './watch_dir.js';
 
 export interface Source_File {
 	id: Path_Id;
@@ -11,12 +18,29 @@ export type Cleanup_Watch = () => void;
 
 export type On_Filer_Change = (change: Watcher_Change, source_file: Source_File) => void;
 
+export interface Options {
+	watch_dir_options?: Partial<Watch_Dir_Options>;
+}
+
 // TODO BLOCK use `watch_dir` - maybe also `search_fs` for non-watch cases? do we have any of those?
 // TODO BLOCK lazy init - should be able to create the class without doing any significant work
 export class Filer {
 	files: Map<Path_Id, Source_File> = new Map();
 
 	watcher: Watch_Node_Fs | undefined;
+	watch_dir_options: Partial<Watch_Dir_Options>;
+
+	constructor(options: Options = EMPTY_OBJECT) {
+		this.watch_dir_options = options.watch_dir_options ?? EMPTY_OBJECT;
+	}
+
+	// TODO BLOCK program reactively? maybe as a followup?
+	#watching: Watch_Node_Fs | undefined;
+	// TODO BLOCK rename?
+	#update_watcher() {
+		if (this.#watching) return;
+		this.#watching = watch_dir({...this.watch_dir_options, on_change: this.on_change}); // TODO maybe make `watch_dir` an option instead of accepting options?
+	}
 
 	get_by_id = (id: Path_Id): Source_File | undefined => {
 		return this.files.get(id);
@@ -42,17 +66,22 @@ export class Filer {
 		return found;
 	};
 
-	listeners: Set<On_Filer_Change> = new Set();
+	#listeners: Set<On_Filer_Change> = new Set();
 
 	watch = (listener: On_Filer_Change): Cleanup_Watch => {
-		this.listeners.add(listener);
+		this.#listeners.add(listener);
+		this.#update_watcher();
 		return () => {
-			this.listeners.delete(listener);
+			this.#listeners.delete(listener);
 		};
 	};
 
 	close = async (): Promise<void> => {
-		this.listeners.clear();
+		this.#listeners.clear();
 		await this.watcher?.close();
+		if (this.#watching) {
+			await this.#watching.close();
+			this.#watching = undefined;
+		}
 	};
 }
