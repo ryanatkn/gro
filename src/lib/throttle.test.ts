541439dec318623adbc606ad6c624677b28e3e7b
Ryan Atkinson <mail@ryanatkn.com>
2024-10-03 15:23:50 -0600

    wip

--- src/lib/throttle.test.ts
+++ src/lib/throttle.test.ts
@@ -11,6 +11,7 @@ test('throttles calls to a function', async () => {
 		await wait();
 		results.push(name + '_done');
 	}, 0);
+
 	const promise_a = fn('a');
 	const promise_b = fn('b'); // discarded
 	const promise_c = fn('c'); // discarded
@@ -21,53 +22,45 @@ test('throttles calls to a function', async () => {
 	assert.is(promise_b, promise_d);
 	assert.equal(results, ['a_run']);
 
-	// await promise_a;
+	await promise_a;
 
-	// assert.equal(results, ['a_run', 'a_done']);
+	assert.equal(results, ['a_run', 'a_done']);
+
+	await wait();
+
+	assert.equal(results, ['a_run', 'a_done', 'd_run']);
 
 	await promise_b;
 
 	assert.equal(results, ['a_run', 'a_done', 'd_run', 'd_done']);
-
-	const promise_e = fn('e'); // discarded
-	const promise_f = fn('f');
-
-	assert.ok(promise_d !== promise_e);
-	assert.is(promise_e, promise_f);
-	assert.equal(results, ['a_run', 'a_done', 'd_run', 'd_done']); // delayed
-
-	await wait();
-
-	assert.equal(results, ['a_run', 'a_done', 'd_run', 'd_done', 'f_run']);
-
-	await promise_e;
-
-	assert.equal(results, ['a_run', 'a_done', 'd_run', 'd_done', 'f_run', 'f_done']);
-
-	const promise_g = fn('g');
-
-	assert.equal(results, ['a_run', 'a_done', 'd_run', 'd_done', 'f_run', 'f_done']); // delayed
-
-	await wait();
-
-	assert.equal(results, ['a_run', 'a_done', 'd_run', 'd_done', 'f_run', 'f_done', 'g_run']);
-
-	await promise_g;
-
-	assert.equal(results, [
-		'a_run',
-		'a_done',
-		'd_run',
-		'd_done',
-		'f_run',
-		'f_done',
-		'g_run',
-		'g_done',
-	]);
 });
 
-test.only('throttles calls to a function with leading = false', async () => {
-	console.log('\n\n\nTEST 2\n\n');
+test('calls functions in sequence', async () => {
+	const results: string[] = [];
+	const fn = throttle(async (name: string) => {
+		results.push(name + '_run');
+		await wait();
+		results.push(name + '_done');
+	}, 0);
+
+	const promise_a = fn('a');
+
+	assert.equal(results, ['a_run']);
+
+	await promise_a;
+
+	assert.equal(results, ['a_run', 'a_done']);
+
+	const promise_b = fn('b');
+
+	assert.ok(promise_a !== promise_b);
+
+	await promise_b;
+
+	assert.equal(results, ['a_run', 'a_done', 'b_run', 'b_done']);
+});
+
+test('throttles calls to a function with leading = false', async () => {
 	const results: string[] = [];
 	const fn = throttle(
 		async (name: string) => {
@@ -89,26 +82,39 @@ test.only('throttles calls to a function with leading = false', async () => {
 	assert.is(promise_a, promise_d);
 	assert.equal(results, []); // No immediate execution
 
-	// await wait(); // Wait for the delay
+	await wait();
 
-	// assert.equal(results, ['d_run']);
+	assert.equal(results, ['d_run']);
 
-	console.log(`promise_a`, promise_a);
 	await promise_a; // All promises resolve to the same result
 
 	assert.equal(results, ['d_run', 'd_done']);
 
 	const promise_e = fn('e');
 	assert.ok(promise_a !== promise_e);
-	assert.equal(results, ['d_run', 'd_done']); // Delayed execution
+	assert.equal(results, ['d_run', 'd_done']);
 
-	await wait(); // Wait for the delay
+	await wait();
 
 	assert.equal(results, ['d_run', 'd_done', 'e_run']);
 
 	await promise_e;
 
 	assert.equal(results, ['d_run', 'd_done', 'e_run', 'e_done']);
+
+	const promise_f = fn('f'); // discarded
+	const promise_g = fn('g');
+	assert.ok(promise_e !== promise_f);
+	assert.ok(promise_f === promise_g);
+	assert.equal(results, ['d_run', 'd_done', 'e_run', 'e_done']);
+
+	await wait();
+
+	assert.equal(results, ['d_run', 'd_done', 'e_run', 'e_done', 'g_run']);
+
+	await promise_g;
+
+	assert.equal(results, ['d_run', 'd_done', 'e_run', 'e_done', 'g_run', 'g_done']);
 });
 
 test.run();
--- src/lib/throttle.ts
+++ src/lib/throttle.ts
@@ -1,7 +1,3 @@
-import {wait} from '@ryanatkn/belt/async.js';
-
-// TODO maybe support non-promise return values?
-
 /**
  * Throttles calls to a callback that returns a void promise.
  * Immediately invokes the callback on the first call.
@@ -11,69 +7,57 @@ import {wait} from '@ryanatkn/belt/async.js';
  * In other words, all calls and their args are discarded
  * during the pending window except for the most recent.
  * Unlike debouncing, this calls the throttled callback
- * both on the leading and trailing edges of the delay window.
+ * both on the leading and trailing edges of the delay window,
+ * and this can be customized by setting `leading` to `false`.
  * It also differs from a queue where every call to the throttled callback eventually runs.
  * @param cb - any function that returns a void promise
  * @param delay - delay this many milliseconds between the pending call finishing and the next starting
+ * @param leading - if `true`, the default, the callback is called immediately
  * @returns same as `cb`
  */
-let i = 0;
 export const throttle = <T extends (...args: any[]) => Promise<void>>(
 	cb: T,
 	delay = 0,
 	leading = true,
 ): T => {
-	const id = i++;
 	let pending_promise: Promise<void> | null = null;
 	let next_args: any[] | null = null;
 	let next_promise: Promise<void> | null = null;
 	let next_promise_resolve: ((value: any) => void) | null = null;
 
 	const defer = (args: any[]): Promise<void> => {
-		console.log(id, '[defer]');
 		next_args = args;
 		if (!next_promise) {
-			console.log(id, '[defer] creating promise');
 			next_promise = new Promise((resolve) => {
 				next_promise_resolve = resolve;
 			});
+			setTimeout(flush, delay);
 		}
 		return next_promise;
 	};
 
 	const flush = async (): Promise<void> => {
-		console.log(id, '[flush]');
 		if (!next_promise_resolve) return;
-		console.log(id, '[flush] calling');
 		const result = await call(next_args!);
-		console.log(id, '[flush] called');
 		next_args = null;
 		next_promise = null;
 		const resolve = next_promise_resolve;
 		next_promise_resolve = null;
-		console.log(id, '[flush] resolving');
 		resolve(result); // resolve last to prevent synchronous call issues
 	};
 
 	const call = (args: any[]): Promise<any> => {
-		console.log(id, '[call]');
 		pending_promise = cb(...args);
-		void pending_promise.then(async () => {
-			console.log(id, '[call] inside pending');
-			await wait(delay);
-			console.log(id, '[call] inside pending after wait');
+		void pending_promise.then(() => {
 			pending_promise = null;
-			await flush();
 		});
 		return pending_promise;
 	};
 
 	return ((...args) => {
 		if (pending_promise || !leading) {
-			console.log(id, 'cb defer');
 			return defer(args);
 		} else {
-			console.log(id, 'cb call');
 			return call(args);
 		}
 	}) as T;
