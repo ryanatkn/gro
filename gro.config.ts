37bc0018bb4a2bda1dab942b338a54ac461ed790
Ryan Atkinson <mail@ryanatkn.com>
2024-10-08 14:49:30 -0600

    wip

--- gro.config.ts
+++ gro.config.ts
@@ -1,3 +1,6 @@
+// TODO see below
+// import {gro_plugin_moss} from '@ryanatkn/moss/gro_plugin_moss.js';
+
 import {create_empty_gro_config} from './src/lib/gro_config.js';
 import {gro_plugin_sveltekit_library} from './src/lib/gro_plugin_sveltekit_library.js';
 import {gro_plugin_sveltekit_app} from './src/lib/gro_plugin_sveltekit_app.js';
@@ -14,6 +17,9 @@ const config = create_empty_gro_config();
 config.plugins = () => [
 	gro_plugin_sveltekit_library(),
 	gro_plugin_sveltekit_app(),
+	// TODO how to do this? moss imports gro, so it needs to exist in node_modules,
+	// maybe the fix is to add an exception for gro in the loader if the project is gro?
+	// gro_plugin_moss() as any,
 	gro_plugin_gen(),
 ];
 
