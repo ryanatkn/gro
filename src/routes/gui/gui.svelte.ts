89d93e5eaa3f3633bb22bf2b670b52bec9b47e4d
Ryan Atkinson <mail@ryanatkn.com>
2024-10-13 01:25:24 -0600

    wip

--- src/routes/gui/gui.svelte.ts
+++ src/routes/gui/gui.svelte.ts
@@ -1,8 +1,10 @@
 import {SvelteMap} from 'svelte/reactivity';
+import {create_deferred, type Deferred} from '@ryanatkn/belt/async.js';
 
 import type {Source_File} from '../../lib/filer.js';
 import type {Path_Id} from '../../lib/path.js';
 import type {Gui_Client} from './gui_client.js';
+import type {Prompt_Response_Message} from './gui_message.js';
 
 export interface Options {
 	client: Gui_Client;
@@ -13,16 +15,31 @@ export class Gui {
 
 	client: Gui_Client;
 
-	pending_prompts = $state(false); // TODO refactor
+	pending_prompts: SvelteMap<string, Deferred<Prompt_Response_Message>> = new SvelteMap();
 
 	constructor(options: Options) {
 		console.log('[gui] creating');
 		this.client = options.client;
 	}
 
-	send_prompt(text: string): void {
+	async send_prompt(text: string): Promise<void> {
 		// TODO need ids, and then the response promise, tracking by text isn't robust to duplicates
 		this.client.send({type: 'send_prompt', text});
-		this.pending_prompt = true; // TODO
+		const deferred = create_deferred<Prompt_Response_Message>();
+		this.pending_prompts.set(text, deferred);
+		const response = await deferred.promise;
+		console.log(`prompt response`, response);
+		this.pending_prompts.delete(text);
+	}
+
+	// TODO BLOCK do this somehow
+	receive_prompt_response(message: Prompt_Response_Message) {
+		const pending = this.pending_prompts.get(message.text);
+		if (!pending) {
+			console.error('expected pending', message);
+			return;
+		}
+		pending.resolve(message);
+		this.pending_prompts.delete(message.text); // deleting intentionally after resolving to maybe avoid a corner case loop of sending the same prompt again
 	}
 }
