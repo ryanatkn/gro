8114b9dd88d771672566c29b92e3aa05ae5acdab
Ryan Atkinson <mail@ryanatkn.com>
2024-10-25 18:05:05 -0600

    wip

--- src/lib/filer.ts
+++ src/lib/filer.ts
@@ -112,30 +112,21 @@ export class Filer {
 			// TODO logic is duplicated from loader
 			const path = map_sveltekit_aliases(specifier, aliases);
 
-			// The specifier `path` has now been mapped to its final form, so we can inspect it.
-			if (path[0] === '.' || path[0] === '/') {
-				const {path_id} = resolve_specifier(path, dir);
-				dependencies_removed.delete(path_id);
-				if (!dependencies_before.has(path_id)) {
-					const d = this.get_or_create(path_id);
-					file.dependencies.set(d.id, d);
-					d.dependents.set(file.id, file);
-				}
-			}
 			// TODO BLOCK should we have a filter for a subset of the node_modules so it doesn't load everything?
 			// TODO BLOCK include `external: true`
-			// TODO BLOCK this doesn't work
-			// const resolved =
-			// 	path[0] === '.' || path[0] === '/'
-			// 		? resolve_specifier(path, dir)
-			// 		: resolve_node_specifier(path, dir);
-			// const {path_id} = resolved;
-			// dependencies_removed.delete(path_id);
-			// if (!dependencies_before.has(path_id)) {
-			// 	const d = this.get_or_create(path_id);
-			// 	file.dependencies.set(d.id, d);
-			// 	d.dependents.set(file.id, file);
-			// }
+			// TODO BLOCK test this
+			// The specifier `path` has now been mapped to its final form, so we can inspect it.
+			const resolved =
+				path[0] === '.' || path[0] === '/'
+					? resolve_specifier(path, dir)
+					: resolve_node_specifier(path, dir);
+			const {path_id} = resolved;
+			dependencies_removed.delete(path_id);
+			if (!dependencies_before.has(path_id)) {
+				const d = this.get_or_create(path_id);
+				file.dependencies.set(d.id, d);
+				d.dependents.set(file.id, file);
+			}
 		}
 
 		// update any removed dependencies
