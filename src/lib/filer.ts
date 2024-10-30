1be8c4ce4af96012e0f688ec5373e707fd4e7140
Ryan Atkinson <mail@ryanatkn.com>
2024-10-30 12:26:54 -0600

    wip

--- src/lib/filer.ts
+++ src/lib/filer.ts
@@ -25,7 +25,6 @@ const aliases = Object.entries(default_sveltekit_config.alias);
 
 export interface Source_File {
 	id: Path_Id;
-	// TODO add // mtime: number;
 	/**
 	 * `null` contents means it doesn't exist.
 	 * We create the file in memory to track its dependents regardless of its existence on disk.
