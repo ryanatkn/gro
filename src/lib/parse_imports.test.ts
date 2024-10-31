f1403b232032f73cf52a6085c38d1c6f6e0e4c00
Ryan Atkinson <mail@ryanatkn.com>
2024-10-31 03:54:14 -0600

    wip

--- src/lib/parse_imports.test.ts
+++ src/lib/parse_imports.test.ts
@@ -1,19 +1,17 @@
 import {test} from 'uvu';
 import * as assert from 'uvu/assert';
 
-import {init_lexer, parse_imports} from './parse_imports.js';
-
-await init_lexer();
+import {parse_imports} from './parse_imports.js';
 
 test('parse js imports', () => {
 	const parsed = parse_imports(
 		'a.js',
 		`
-    const a = 5;
-    import {foo} from 'static_import';
-    await import('dynamic_import');
-    const b = {};
-  `,
+      const a = 5;
+      import {foo} from 'static_import';
+      await import('dynamic_import');
+      const b = {};
+    `,
 	);
 	assert.equal(parsed, ['static_import', 'dynamic_import']);
 });
@@ -22,27 +20,27 @@ test('parse ts imports', () => {
 	const parsed = parse_imports(
 		'a.ts',
 		`
-    const a: 5 = 5;
-    type A = 123;
-    interface B {
-      b: 456;
-    }
-    import {foo} from 'static_import';
-    await import('dynamic_import');
-    const b: {} = {};
-    export type {G} from './g';
-  `,
+      const a: 5 = 5;
+      type A = 123;
+      interface B {
+        b: 456;
+      }
+      import {foo} from 'static_import';
+      await import('dynamic_import');
+      const b: {} = {};
+      export type {G} from 'exported_import';
+    `,
 	);
-	assert.equal(parsed, ['static_import', 'dynamic_import']);
+	assert.equal(parsed, ['static_import', 'dynamic_import', 'exported_import']);
 });
 
 test('parse ts imports and omit types', () => {
 	const parsed = parse_imports(
 		'a.ts',
 		`
-    import type {foo} from 'static_import';
-    await import('dynamic_import');
-  `,
+      import type {foo} from 'static_import';
+      await import('dynamic_import');
+    `,
 	);
 	assert.equal(parsed, ['dynamic_import']);
 });
@@ -51,9 +49,9 @@ test('parse ts imports and include types', () => {
 	const parsed = parse_imports(
 		'a.ts',
 		`
-    import type {foo} from 'static_import';
-    await import('dynamic_import');
-  `,
+      import type {foo} from 'static_import';
+      await import('dynamic_import');
+    `,
 		false,
 	);
 	assert.equal(parsed, ['static_import', 'dynamic_import']);
@@ -83,7 +81,8 @@ test('parse svelte imports', () => {
         const b: {} = {};
       </script>
       
-      222`,
+      222
+    `,
 	);
 	assert.equal(parsed, [
 		'static_import_module_context',
@@ -96,12 +95,14 @@ test('parse svelte imports', () => {
 test('parse plain JS svelte imports', () => {
 	const parsed = parse_imports(
 		'a.svelte',
-		`<script>
-      const a = 5;
-      import {foo3} from 'static_import';
-      await import('dynamic_import');
-      const b3 = {};
-    </script>`,
+		`
+      <script>
+        const a = 5;
+        import {foo3} from 'static_import';
+        await import('dynamic_import');
+        const b3 = {};
+      </script>
+    `,
 	);
 	assert.equal(parsed, ['static_import', 'dynamic_import']);
 });
@@ -110,16 +111,15 @@ test('parse empty imports', () => {
 	const parsed = parse_imports(
 		'a.ts',
 		`
-    const a: 5 = 5;
-    type A = 123;
-    interface B {
-      b: 456;
-    }
-    import {foo} from '';
-    await import('');
-    const b: {} = {};
-    export type {G} from './g';
-  `,
+      const a: 5 = 5;
+      type A = 123;
+      interface B {
+        b: 456;
+      }
+      import {foo} from '';
+      await import('');
+      const b: {} = {};
+    `,
 	);
 	assert.equal(parsed, []);
 });
