ff820b03fc7bd350d793125611bc53639e8b163f
Ryan Atkinson <mail@ryanatkn.com>
2024-11-04 10:51:14 -0700

    enable svelte inspector

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,7 @@ import adapter from '@sveltejs/adapter-static';
 export default {
 	preprocess: [vitePreprocess()],
 	compilerOptions: {runes: true},
-	// TODO ideally this would use the default but it conflicts with ctrl+shift+c in Chrome,
-	// but it's strange to me that I'm resetting it to what the Mac default is
-	// vitePlugin: {inspector: {toggleKeyCombo: 'meta-shift'}}, // docs: https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/inspector.md
+	vitePlugin: {inspector: true},
 	kit: {
 		adapter: adapter(),
 		paths: {relative: false}, // use root-absolute paths: https://kit.svelte.dev/docs/configuration#paths
