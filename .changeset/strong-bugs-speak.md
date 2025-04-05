37900e5ab3589b08965bbb09469b1bdab7bd88de
Ryan Atkinson <mail@ryanatkn.com>
2025-04-04 23:06:48 -0600

    fix sveltekit shim regexp

---
+++ .changeset/strong-bugs-speak.md
@@ -0,0 +1,5 @@
+---
+'@ryanatkn/gro': patch
+---
+
+fix sveltekit shim regexp
--- src/lib/esbuild_plugin_sveltekit_shim_alias.ts
+++ src/lib/esbuild_plugin_sveltekit_shim_alias.ts
@@ -14,11 +14,13 @@ export const esbuild_plugin_sveltekit_shim_alias = ({
 	name: 'sveltekit_shim_alias',
 	setup: (build) => {
 		const aliases: Record<string, string> = {$lib: 'src/lib', ...alias};
-		const alias_regexp_prefixes = Object.keys(aliases).map((a) => escape_regexp(a));
-		const filter = new RegExp('^(' + alias_regexp_prefixes.join('|') + ')', 'u');
+		// Create a Go-compatible regexp
+		const filter = new RegExp(`^(?:${Object.keys(aliases).map(escape_regexp).join('|')})`);
 		build.onResolve({filter}, async (args) => {
 			const {path, ...rest} = args;
-			const prefix = filter.exec(path)![1];
+			// Find which alias prefix matches
+			const prefix = Object.keys(aliases).find((key) => path.startsWith(key));
+			if (!prefix) return null;
 			return build.resolve(join(dir, aliases[prefix] + path.substring(prefix.length)), rest);
 		});
 	},
