c9bf2c32afe06bf4f38b70b37869988277c3a941
Ryan Atkinson <mail@ryanatkn.com>
2025-04-11 08:07:46 -0600

    wip

--- src/lib/package_json.ts
+++ src/lib/package_json.ts
@@ -30,36 +30,30 @@ export const transform_empty_object_to_undefined = <T>(val: T): T | undefined =>
 
 export const Package_Json_Repository = z.union([
 	z.string(),
-	z
-		.interface({
-			type: z.string(),
-			url: Url,
-			directory: z.string().optional(),
-		})
-		.passthrough(),
+	z.looseInterface({
+		type: z.string(),
+		url: Url,
+		directory: z.string().optional(),
+	}),
 ]);
 export type Package_Json_Repository = z.infer<typeof Package_Json_Repository>;
 
 export const Package_Json_Author = z.union([
 	z.string(),
-	z
-		.interface({
-			name: z.string(),
-			email: Email.optional(),
-			url: Url.optional(),
-		})
-		.passthrough(),
+	z.looseInterface({
+		name: z.string(),
+		email: Email.optional(),
+		url: Url.optional(),
+	}),
 ]);
 export type Package_Json_Author = z.infer<typeof Package_Json_Author>;
 
 export const Package_Json_Funding = z.union([
 	z.string(),
-	z
-		.interface({
-			type: z.string(),
-			url: Url,
-		})
-		.passthrough(),
+	z.looseInterface({
+		type: z.string(),
+		url: Url,
+	}),
 ]);
 export type Package_Json_Funding = z.infer<typeof Package_Json_Funding>;
 
@@ -97,81 +91,75 @@ export type Package_Json_Exports = z.infer<typeof Package_Json_Exports>;
 /**
  * @see https://docs.npmjs.com/cli/v10/configuring-npm/package-json
  */
-export const Package_Json = z
-	.interface({
-		// according to the npm docs, `name` and `version` are the only required properties
-		name: z.string(),
-		version: z.string(),
-		private: z
-			.boolean()
-			.meta({description: 'disallow publishing to the configured registry'})
-			.optional(),
-		public: z
-			.boolean()
-			.meta({
-				description:
-					'a Gro extension that enables publishing `.well-known/package.json` and `.well-known/src`',
-			})
-			.optional(),
-		description: z.string().optional(),
-		motto: z
-			.string()
-			.meta({description: "a Gro extension that's a short phrase that represents this project"})
-			.optional(),
-		glyph: z
-			.string()
-			.meta({
-				description:
-					"a Gro extension that's a single unicode character that represents this project",
-			})
-			.refine((v) => count_graphemes(v) === 1, 'must be a single unicode character')
-			.optional(),
-		logo: z
-			.string()
-			.meta({
-				description:
-					"a Gro extension that's a link relative to the `homepage` to an image that represents this project",
-			})
-			.optional(),
-		logo_alt: z
-			.string()
-			.meta({description: "a Gro extension that's the alt text for the `logo`"})
-			.optional(),
-		license: z.string().optional(),
-		scripts: z.record(z.string(), z.string()).optional(),
-		homepage: Url.optional(),
-		author: z.union([z.string(), Package_Json_Author.optional()]),
-		repository: z.union([z.string(), Url, Package_Json_Repository]).optional(),
-		contributors: z.array(z.union([z.string(), Package_Json_Author])).optional(),
-		bugs: z
-			.union([
-				z.string(),
-				z.interface({url: Url.optional(), email: Email.optional()}).passthrough(),
-			])
-			.optional(),
-		funding: z
-			.union([Url, Package_Json_Funding, z.array(z.union([Url, Package_Json_Funding]))])
-			.optional(),
-		keywords: z.array(z.string()).optional(),
+export const Package_Json = z.looseInterface({
+	// according to the npm docs, `name` and `version` are the only required properties
+	name: z.string(),
+	version: z.string(),
+	private: z
+		.boolean()
+		.meta({description: 'disallow publishing to the configured registry'})
+		.optional(),
+	public: z
+		.boolean()
+		.meta({
+			description:
+				'a Gro extension that enables publishing `.well-known/package.json` and `.well-known/src`',
+		})
+		.optional(),
+	description: z.string().optional(),
+	motto: z
+		.string()
+		.meta({description: "a Gro extension that's a short phrase that represents this project"})
+		.optional(),
+	glyph: z
+		.string()
+		.meta({
+			description: "a Gro extension that's a single unicode character that represents this project",
+		})
+		.refine((v) => count_graphemes(v) === 1, 'must be a single unicode character')
+		.optional(),
+	logo: z
+		.string()
+		.meta({
+			description:
+				"a Gro extension that's a link relative to the `homepage` to an image that represents this project",
+		})
+		.optional(),
+	logo_alt: z
+		.string()
+		.meta({description: "a Gro extension that's the alt text for the `logo`"})
+		.optional(),
+	license: z.string().optional(),
+	scripts: z.record(z.string(), z.string()).optional(),
+	homepage: Url.optional(),
+	author: z.union([z.string(), Package_Json_Author.optional()]),
+	repository: z.union([z.string(), Url, Package_Json_Repository]).optional(),
+	contributors: z.array(z.union([z.string(), Package_Json_Author])).optional(),
+	bugs: z
+		.union([z.string(), z.looseInterface({url: Url.optional(), email: Email.optional()})])
+		.optional(),
+	funding: z
+		.union([Url, Package_Json_Funding, z.array(z.union([Url, Package_Json_Funding]))])
+		.optional(),
+	keywords: z.array(z.string()).optional(),
 
-		type: z.string().optional(),
-		engines: z.record(z.string(), z.string()).optional(),
-		os: z.array(z.string()).optional(),
-		cpu: z.array(z.string()).optional(),
+	type: z.string().optional(),
+	engines: z.record(z.string(), z.string()).optional(),
+	os: z.array(z.string()).optional(),
+	cpu: z.array(z.string()).optional(),
 
-		dependencies: z.record(z.string(), z.string()).optional(),
-		devDependencies: z.record(z.string(), z.string()).optional(),
-		peerDependencies: z.record(z.string(), z.string()).optional(),
-		peerDependenciesMeta: z.record(z.string(), z.interface({optional: z.boolean()})).optional(),
-		optionalDependencies: z.record(z.string(), z.string()).optional(),
+	dependencies: z.record(z.string(), z.string()).optional(),
+	devDependencies: z.record(z.string(), z.string()).optional(),
+	peerDependencies: z.record(z.string(), z.string()).optional(),
+	peerDependenciesMeta: z.record(z.string(), z.interface({optional: z.boolean()})).optional(),
+	optionalDependencies: z.record(z.string(), z.string()).optional(),
 
-		bin: z.record(z.string(), z.string()).optional(),
-		sideEffects: z.array(z.string()).optional(),
-		files: z.array(z.string()).optional(),
-		main: z.string().optional(),
-		exports: Package_Json_Exports.transform(transform_empty_object_to_undefined).optional(),
-	})
-	.passthrough();
+	bin: z.record(z.string(), z.string()).optional(),
+	sideEffects: z.array(z.string()).optional(),
+	files: z.array(z.string()).optional(),
+	main: z.string().optional(),
+	exports: Package_Json_Exports.transform(transform_empty_object_to_undefined).optional(),
+});
 export type Package_Json = z.infer<typeof Package_Json>;
 
 export type Map_Package_Json = (
--- src/lib/src_json.ts
+++ src/lib/src_json.ts
@@ -11,23 +11,19 @@ import {
 	type Package_Json_Exports,
 } from './package_json.js';
 
-export const Src_Module_Declaration = z
-	.interface({
-		name: z.string(), // the export identifier
-		// TODO these are poorly named, and they're somewhat redundant with `kind`,
-		// they were added to distinguish `VariableDeclaration` functions and non-functions
-		kind: z.enum(['type', 'function', 'variable', 'class']).nullable(),
-		// code: z.string(), // TODO experiment with `getType().getText()`, some of them return the same as `name`
-	})
-	.passthrough();
+export const Src_Module_Declaration = z.looseInterface({
+	name: z.string(), // the export identifier
+	// TODO these are poorly named, and they're somewhat redundant with `kind`,
+	// they were added to distinguish `VariableDeclaration` functions and non-functions
+	kind: z.enum(['type', 'function', 'variable', 'class']).nullable(),
+	// code: z.string(), // TODO experiment with `getType().getText()`, some of them return the same as `name`
+});
 export type Src_Module_Declaration = z.infer<typeof Src_Module_Declaration>;
 
-export const Src_Module = z
-	.interface({
-		path: z.string(),
-		declarations: z.array(Src_Module_Declaration),
-	})
-	.passthrough();
+export const Src_Module = z.looseInterface({
+	path: z.string(),
+	declarations: z.array(Src_Module_Declaration),
+});
 export type Src_Module = z.infer<typeof Src_Module>;
 
 export const Src_Modules = z.record(z.string(), Src_Module);
@@ -36,13 +32,11 @@ export type Src_Modules = z.infer<typeof Src_Modules>;
 /**
  * @see https://github.com/ryanatkn/gro/blob/main/src/docs/gro_plugin_sveltekit_app.md#well-known-src
  */
-export const Src_Json = z
-	.interface({
-		name: z.string(), // same as Package_Json
-		version: z.string(), // same as Package_Json
-		modules: Src_Modules.transform(transform_empty_object_to_undefined).optional(),
-	})
-	.passthrough();
+export const Src_Json = z.looseInterface({
+	name: z.string(), // same as Package_Json
+	version: z.string(), // same as Package_Json
+	modules: Src_Modules.transform(transform_empty_object_to_undefined).optional(),
+});
 export type Src_Json = z.infer<typeof Src_Json>;
 
 export type Map_Src_Json = (src_json: Src_Json) => Src_Json | null | Promise<Src_Json | null>;
