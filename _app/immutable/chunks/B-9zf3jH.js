aec3b5326f3fd45317cbe0736a80c089283dc107
Ryan Atkinson <mail@ryanatkn.com>
2025-04-30 16:03:10 -0600

    deployment

---
+++ _app/immutable/chunks/B-9zf3jH.js
@@ -0,0 +1,1 @@
+import{l as t,u as e,c,a as u}from"./B0j3FEyJ.js";function a(o){c===null&&t(),e(()=>{const n=u(o);if(typeof n=="function")return n})}export{a as o};
--- _app/immutable/nodes/4.DXR7R4J5.js
+++ _app/immutable/nodes/4.DXR7R4J5.js
@@ -1,3 +1,3 @@
-import{t as a,b as t}from"../chunks/DVVIXG-p.js";var i=a(`<div class="markup svelte-18vk2t"><p>Gro previously had a dev server with an experimental frontend for visualizing and working with
+import{t as a,b as t}from"../chunks/nvI3nRhq.js";var i=a(`<div class="markup svelte-18vk2t"><p>Gro previously had a dev server with an experimental frontend for visualizing and working with
 		build data. And then SvelteKit and Vite came along!</p> <p>It was removed in <a href="https://github.com/ryanatkn/gro/pull/321">PR #321</a> and is archived
 		here: <a href="https://github.com/spiderspace/gro/tree/archive/devserver">https://github.com/spiderspace/gro/tree/archive/devserver</a></p> <p>Vite plugins should be used going forward.</p></div>`);function o(e){var r=i();t(e,r)}export{o as component};
