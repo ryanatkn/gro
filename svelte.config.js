6f9840473e1c8b357ca04d64f313e42083def056
Ryan Atkinson <mail@ryanatkn.com>
2025-04-30 08:09:24 -0600

    add csp

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
+import {create_csp_directives} from '@ryanatkn/fuz/csp.js';
+import {csp_trusted_sources_of_ryanatkn} from '@ryanatkn/fuz/csp_of_ryanatkn.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -10,5 +12,10 @@ export default {
 		adapter: adapter(),
 		paths: {relative: false}, // use root-absolute paths: https://kit.svelte.dev/docs/configuration#paths
 		alias: {$routes: 'src/routes'},
+		csp: {
+			directives: create_csp_directives({
+				trusted_sources: csp_trusted_sources_of_ryanatkn,
+			}),
+		},
 	},
 };
