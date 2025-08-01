5aeea3f25c4cecedf4b51afe67954de49f08dd50
Ryan Atkinson <mail@ryanatkn.com>
2025-08-01 12:26:08 -0400

    wip

--- src/lib/sveltekit_shim_app_paths.ts
+++ src/lib/sveltekit_shim_app_paths.ts
@@ -8,9 +8,12 @@
  * @see https://github.com/nodejs/loaders for details about the forthcoming virtual file support
  */
 
-import type {resolveRoute as base_resolveRoute} from '$app/paths';
+import type {resolve as base_resolve, resolveRoute as base_resolveRoute} from '$app/paths';
 import {noop} from '@ryanatkn/belt/function.js';
 
 export const assets = '';
+/** @deprecated */
 export const base = '';
+export const resolve: typeof base_resolve = (v) => ('/' + v.replace(/^\//, '')) as any; // TODO needs to use SvelteKit config base, should we just import it?
+/** @deprecated */
 export const resolveRoute: typeof base_resolveRoute = noop;
