0369b1134cf3a9f292f918b03093a339fc8c284b
Ryan Atkinson <mail@ryanatkn.com>
2025-08-03 22:26:18 -0400

    wip

--- src/lib/test.task.ts
+++ src/lib/test.task.ts
@@ -1,9 +1,11 @@
 import {styleText as st} from 'node:util';
 import {z} from 'zod';
+import {spawn_cli} from '@ryanatkn/gro/cli.js';
 
 import {Task_Error, type Task} from './task.ts';
 import {paths} from './paths.ts';
 import {find_cli} from './cli.ts';
+import {has_dep} from './package_json.ts';
 
 export const Args = z.strictObject({
 	_: z.array(z.string()).meta({description: 'file patterns to test'}).default([`\\.test\\.ts$`]), // TODO maybe use uvu's default instead of being restrictive?
@@ -22,29 +24,37 @@ export type Args = z.infer<typeof Args>;
 export const task: Task<Args> = {
 	summary: 'run tests with uvu',
 	Args,
-	run: async ({args, log}): Promise<void> => {
+	run: async ({args}): Promise<void> => {
 		const {_: patterns, bail, cwd, ignore} = args;
 
-		if (!find_cli('uvu')) {
-			log.warn(st('yellow', 'uvu is not installed, skipping tests'));
-			return;
-		}
+		if (has_dep('vitest')) {
+			if (!find_cli('vitest')) {
+				throw new Task_Error('vitest is a dependency but not installed; run `npm i`?');
+			}
+			const spawned = await spawn_cli('vitest', ['run', ...patterns, '--dir', 'src']); // TODO proper forwarding
+			if (!spawned?.ok) {
+				throw new Task_Error(`vitest failed with exit code ${spawned?.code}`);
+			}
+		} else if (has_dep('uvu')) {
+			if (!find_cli('uvu')) {
+				throw new Task_Error('uvu is a dependency but not installed; run `npm i`?');
+			}
 
-		const [{run}, {parse}] = await Promise.all([import('uvu/run'), import('uvu/parse')]);
+			const [{run}, {parse}] = await Promise.all([import('uvu/run'), import('uvu/parse')]);
 
-		// uvu doesn't work with esm loaders and TypeScript files,
-		// so we use its `parse` and `run` APIs directly instead of its CLI.
-		// To avoid surprises, we allow any number of patterns in the rest args,
-		// so we call `parse` multiple times because it supports only one.
-		const suites = [];
-		for (const pattern of patterns) {
-			const parsed = await parse(paths.source, pattern, {cwd, ignore}); // eslint-disable-line no-await-in-loop
-			suites.push(...parsed.suites);
-		}
-		await run(suites, {bail});
-
-		if (process.exitCode) {
-			throw new Task_Error('Tests failed.');
+			// uvu doesn't work with esm loaders and TypeScript files,
+			// so we use its `parse` and `run` APIs directly instead of its CLI.
+			// To avoid surprises, we allow any number of patterns in the rest args,
+			// so we call `parse` multiple times because it supports only one.
+			const suites = [];
+			for (const pattern of patterns) {
+				const parsed = await parse(paths.source, pattern, {cwd, ignore}); // eslint-disable-line no-await-in-loop
+				suites.push(...parsed.suites);
+			}
+			await run(suites, {bail});
+			if (process.exitCode) {
+				throw new Task_Error(`uvu failed with exit code ${process.exitCode}`);
+			}
 		}
 	},
 };
