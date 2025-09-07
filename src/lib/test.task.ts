0645fa9bc4a7fe0ecda846fb441cfa7622a415a3
Ryan Atkinson <mail@ryanatkn.com>
2025-09-07 09:44:32 -0400

    wip

--- src/lib/test.task.ts
+++ src/lib/test.task.ts
@@ -9,9 +9,8 @@ import {VITEST_CLI} from './constants.ts';
 import {paths} from './paths.ts';
 
 export const Args = z.strictObject({
-	_: z.array(z.string()).meta({description: 'file patterns to filter tests'}).default([]),
+	_: z.array(z.string()).meta({description: 'file patterns to filter tests'}).default(['.test.']),
 	dir: z.string().meta({description: 'working directory for tests'}).default(paths.source),
-	include: z.string().meta({description: 'file include pattern'}).default('**/*.test.ts'),
 	t: z
 		.string()
 		.meta({description: 'search pattern for test names, same as vitest -t and --testNamePattern'})
@@ -23,28 +22,29 @@ export const task: Task<Args> = {
 	summary: 'run tests with vitest',
 	Args,
 	run: async ({args}): Promise<void> => {
-		const {_: patterns, dir, include, t} = args;
+		const {_: patterns, dir, t} = args;
 
-		if (has_dep(VITEST_CLI)) {
-			if (!find_cli(VITEST_CLI)) {
-				throw new Task_Error('vitest is a dependency but not installed; run `npm i`?');
-			}
-
-			const vitest_args = ['run', ...patterns, '--include', include];
-			if (dir) {
-				vitest_args.push('--dir', dir);
-			}
-			if (t) {
-				vitest_args.push('-t', t);
-			}
-			vitest_args.push(...serialize_args(to_forwarded_args(VITEST_CLI)));
-
-			const spawned = await spawn_cli(VITEST_CLI, vitest_args); // TODO proper forwarding
-			if (!spawned?.ok) {
-				throw new Task_Error(`vitest failed with exit code ${spawned?.code}`);
-			}
-		} else {
+		if (!has_dep(VITEST_CLI)) {
 			throw new Task_Error('no test runner found, install vitest');
+		}
+
+		if (!find_cli(VITEST_CLI)) {
+			throw new Task_Error('vitest is a dependency but not installed; run `npm i`?');
+		}
+
+		const vitest_args = ['run', ...patterns];
+		if (dir) {
+			vitest_args.push('--dir', dir);
+		}
+		if (t) {
+			vitest_args.push('-t', t);
+		}
+		vitest_args.push(...serialize_args(to_forwarded_args(VITEST_CLI)));
+
+		const spawned = await spawn_cli(VITEST_CLI, vitest_args);
+		if (!spawned?.ok) {
+			console.log(`spawned`, spawned);
+			throw new Task_Error(`vitest failed with exit code ${spawned?.code}`);
 		}
 	},
 };
