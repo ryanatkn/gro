ca33f21ad56231f64e081b4cf3ae2f13a527fe20
Ryan Atkinson <mail@ryanatkn.com>
2025-11-24 07:15:31 -0500

    wip

--- src/routes/docs/api/[...module_path]/+page.svelte
+++ src/routes/docs/api/[...module_path]/+page.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
 	import Api_Module from '@ryanatkn/fuz/Api_Module.svelte';
 
-	const {
-		params,
-	}: {
-		params: {module_path?: string};
-	} = $props();
+	const {params} = $props();
 
 	const module_path = $derived(params.module_path ?? '');
 </script>
