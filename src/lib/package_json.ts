05d11f5344678473887b445202f60361c5a096f1
Ryan Atkinson <mail@ryanatkn.com>
2025-12-19 08:02:08 -0500

    wip

--- src/lib/package_json.ts
+++ src/lib/package_json.ts
@@ -51,6 +51,9 @@ export const package_json_load = async (
 	return package_json;
 };
 
+// TODO remove
+export const load_package_json = package_json_load;
+
 export const package_json_sync = async (
 	map_package_json: PackageJsonMapper,
 	log: Logger,
